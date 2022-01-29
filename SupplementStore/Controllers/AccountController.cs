b52f57fa0569a91e8733cc1b7c8eeecc4e6dfb6d
Adrian <mitura55@interia.pl>
2022-01-29 05:30:02 +0100

    Create AccountController with Register actions.

---
+++ SupplementStore/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SupplementStore.ViewModels.Account;
+using System.Threading.Tasks;
+
+namespace SupplementStore.Controllers {
+
+    public class AccountController : Controller {
+
+        UserManager<IdentityUser> UserManager { get; }
+
+        public AccountController(UserManager<IdentityUser> userManager) {
+
+            UserManager = userManager;
+        }
+
+        public IActionResult Register(string returnUrl) {
+
+            ViewBag.ReturnUrl = returnUrl;
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model, string returnUrl) {
+
+            if (ModelState.IsValid) {
+
+                IdentityUser user = new IdentityUser {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+
+                IdentityResult result = await UserManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded) {
+
+                    return LocalRedirect(returnUrl ?? "/");
+                }
+                else {
+
+                    foreach (IdentityError error in result.Errors) {
+
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
