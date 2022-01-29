53f43b20f9b7eca28b79db24d5c93e1de0efbc73
Adrian <mitura55@interia.pl>
2022-01-29 05:39:37 +0100

    Add Login actions to AccountController.

--- SupplementStore/Controllers/AccountController.cs
+++ SupplementStore/Controllers/AccountController.cs
@@ -9,9 +9,46 @@ namespace SupplementStore.Controllers {
 
         UserManager<IdentityUser> UserManager { get; }
 
-        public AccountController(UserManager<IdentityUser> userManager) {
+        SignInManager<IdentityUser> SignInManager { get; }
+
+        public AccountController(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager) {
 
             UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public IActionResult Login(string returnUrl) {
+
+            ViewBag.ReturnUrl = returnUrl;
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel details, string returnUrl) {
+
+            if (ModelState.IsValid) {
+
+                IdentityUser user = await UserManager.FindByEmailAsync(details.Email);
+
+                if (user != null) {
+
+                    await SignInManager.SignOutAsync();
+
+                    Microsoft.AspNetCore.Identity.SignInResult result = await SignInManager.PasswordSignInAsync(user, details.Password, false, false);
+
+                    if (result.Succeeded) {
+
+                        return LocalRedirect(returnUrl ?? "/");
+                    }
+                }
+
+                ModelState.AddModelError(nameof(LoginViewModel.Email), "Invalid user or password");
+            }
+
+            return View(details);
         }
 
         public IActionResult Register(string returnUrl) {
