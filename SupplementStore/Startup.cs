9e5082b7c5ebeddc94d4e3d8bdaedd544a2bcd98
Adrian <mitura55@interia.pl>
2022-01-29 05:17:40 +0100

    Configure the database.

--- SupplementStore/Startup.cs
+++ SupplementStore/Startup.cs
@@ -1,14 +1,32 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SupplementStore.Infrastructure;
 
 namespace SupplementStore {
 
     public class Startup {
 
+        IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) {
+
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+
+            services.AddIdentity<IdentityUser, IdentityRole>(options => {
+                options.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
 
             services.AddMvc();
         }
@@ -23,6 +41,8 @@ namespace SupplementStore {
 
             app.UseStatusCodePages();
             app.UseStaticFiles();
+
+            app.UseAuthentication();
 
             app.UseMvc(routes => {
 
