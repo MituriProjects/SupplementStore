83cc83188a5a2f97c9dd2c64ecf12cd4a248c5ba
Adrian <mitura55@interia.pl>
2022-02-01 19:00:19 +0100

    Create RandomManager.

---
+++ SupplementStore.Tests/RandomManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SupplementStore.Tests {
+
+    static class RandomManager {
+
+        static Random Random { get; } = new Random();
+
+        public static int Next(int max) {
+
+            return Random.Next(max);
+        }
+    }
+}
