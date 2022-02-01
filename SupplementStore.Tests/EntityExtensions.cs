e6b3b013a5d02dc9048ea9932ee3d43762448fc3
Adrian <mitura55@interia.pl>
2022-02-01 18:51:11 +0100

    Create EntityExtensions.

---
+++ SupplementStore.Tests/EntityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using SupplementStore.Domain.Entities;
+using System;
+
+namespace SupplementStore.Tests {
+
+    static class EntityExtensions {
+
+        public static TEntity WithId<TEntity>(this TEntity entity, Guid id)
+            where TEntity : Entity {
+
+            entity.Id = id;
+
+            return entity;
+        }
+    }
+}
