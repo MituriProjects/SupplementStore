48cab39b32b0bc2d68ab9dc309ac7dc4feebdcff
Adrian <mitura55@interia.pl>
2022-02-01 18:47:26 +0100

    Create IClearableDocument.

---
+++ SupplementStore.Tests/IClearableDocument.cs
@@ -0,0 +1,13 @@
+﻿using SupplementStore.Domain.Entities;
+using SupplementStore.Infrastructure;
+
+namespace SupplementStore.Tests {
+
+    interface IClearableDocument {
+        void Clear();
+    }
+
+    interface IClearableDocument<TEntity> : IDocument<TEntity>, IClearableDocument
+        where TEntity : Entity {
+    }
+}
