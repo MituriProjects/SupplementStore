17980de1c63fa659d5ab340a0a7e9ead6effc368
Adrian <mitura55@interia.pl>
2022-02-01 19:00:28 +0100

    Create TestProduct.

---
+++ SupplementStore.Tests/TestProduct.cs
@@ -0,0 +1,50 @@
+﻿using SupplementStore.Domain.Entities.Products;
+using System;
+using System.Linq;
+
+namespace SupplementStore.Tests {
+
+    class TestProduct : Product {
+
+        public TestProduct WithName(string name) {
+
+            Name = name;
+
+            return this;
+        }
+
+        public TestProduct WithPrice(decimal price) {
+
+            Price = price;
+
+            return this;
+        }
+
+        public static TestProduct New() {
+
+            var product = new TestProduct();
+
+            TestDocument<Product>.Add(product);
+
+            return product;
+        }
+
+        public static TestProduct Random() {
+
+            var id = Guid.NewGuid();
+
+            var product = new TestProduct {
+                Id = id,
+                Name = $"TestProduct-{id}",
+                Price = RandomManager.Next(10000) / 100M
+            };
+
+            TestDocument<Product>.Add(product);
+
+            return product;
+        }
+
+        public static TestProduct[] Random(int count) =>
+            new TestProduct[count].Select(e => Random()).ToArray();
+    }
+}
