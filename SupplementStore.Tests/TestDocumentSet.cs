9dffa85c5465826be97157e88bd4d74f07fdda46
Adrian <mitura55@interia.pl>
2022-02-01 18:49:52 +0100

    Create TestDocumentSet.

---
+++ SupplementStore.Tests/TestDocumentSet.cs
@@ -0,0 +1,50 @@
+﻿using SupplementStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SupplementStore.Tests {
+
+    static class TestDocumentSet {
+
+        static List<IClearableDocument> Documents { get; } = new List<IClearableDocument>();
+
+        static Dictionary<Type, Func<IEnumerable<IEnumerable<object>>>> EntityValues { get; } = new Dictionary<Type, Func<IEnumerable<IEnumerable<object>>>>();
+
+        public static void Register<TEntity>(IClearableDocument<TEntity> document)
+            where TEntity : Entity {
+
+            if (EntityValues.TryAdd(typeof(TEntity), () => document.All.Select(e => GetEntityValues(e)))) {
+
+                Documents.Add(document);
+            }
+        }
+
+        public static void ClearDocuments() {
+
+            Documents.ForEach(d => d.Clear());
+
+            Documents.Clear();
+
+            EntityValues.Clear();
+        }
+
+        public static IEnumerable<object> GetValues() {
+
+            return EntityValues
+                .Values
+                .Select(v => v())
+                .SelectMany(e => e)
+                .SelectMany(e => e)
+                .ToList();
+        }
+
+        private static IEnumerable<object> GetEntityValues<TEntity>(TEntity testEntity)
+            where TEntity : Entity {
+
+            return TestEntityHelper
+                .SelectPropertyValues(testEntity, testEntity)
+                .Select(p => p.Value);
+        }
+    }
+}
