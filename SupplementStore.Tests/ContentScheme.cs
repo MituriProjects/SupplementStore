84bbd4355262138dbc952cfba390d5215ac7c4bd
Adrian <mitura55@interia.pl>
2022-02-01 18:55:21 +0100

    Create ContentScheme.

---
+++ SupplementStore.Tests/ContentScheme.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace SupplementStore.Tests {
+
+    public class ContentScheme {
+
+        List<ContentElement> Elements { get; } = new List<ContentElement>();
+
+        public ContentScheme Contains(object value) {
+
+            Elements.Add(new ContentElement(value, true));
+
+            return this;
+        }
+
+        public ContentScheme Lacks(object value) {
+
+            Elements.Add(new ContentElement(value, false));
+
+            return this;
+        }
+
+        public void Examine(string content) {
+
+            foreach (var contentElement in Elements) {
+
+                contentElement.Examine(content);
+            }
+        }
+    }
+}
