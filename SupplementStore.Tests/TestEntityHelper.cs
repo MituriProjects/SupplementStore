d93ea97b29d0bc879c328deca1389f251e28205a
Adrian <mitura55@interia.pl>
2022-02-01 18:48:41 +0100

    Create TestEntityHelper.

---
+++ SupplementStore.Tests/TestEntityHelper.cs
@@ -0,0 +1,49 @@
+﻿using SupplementStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace SupplementStore.Tests {
+
+    static class TestEntityHelper {
+
+        public static IEnumerable<MethodInfo> SelectSetterMethods<TEntity>(TEntity testEntity)
+            where TEntity : Entity {
+
+            if (testEntity == null)
+                throw new ArgumentNullException(nameof(testEntity));
+
+            return testEntity
+                .GetType()
+                .GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                .Where(m => m.Name.StartsWith("With"));
+        }
+
+        public static IEnumerable<KeyValuePair<string, object>> SelectPropertyValues<TEntity>(TEntity entity, TEntity testEntity)
+            where TEntity : Entity {
+
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            if (testEntity == null)
+                throw new ArgumentNullException(nameof(testEntity));
+
+            var setterMethods = SelectSetterMethods(testEntity);
+
+            foreach (var setterMethod in setterMethods) {
+
+                var propertyName = setterMethod.Name.Substring(4);
+
+                var propertyValue = entity
+                    .GetType()
+                    .GetProperty(propertyName)
+                    .GetValue(entity);
+
+                yield return new KeyValuePair<string, object>(propertyName, propertyValue);
+            }
+
+            yield return new KeyValuePair<string, object>("Id", entity.Id);
+        }
+    }
+}
