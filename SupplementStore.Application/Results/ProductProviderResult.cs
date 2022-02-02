7d56b36794f236047a989f92164979d231639dfb
Adrian <mitura55@interia.pl>
2022-02-02 14:03:07 +0100

    Create ProductProviderResult.

---
+++ SupplementStore.Application/Results/ProductProviderResult.cs
@@ -0,0 +1,12 @@
+﻿using SupplementStore.Application.Models;
+using System.Collections.Generic;
+
+namespace SupplementStore.Application.Results {
+
+    public class ProductProviderResult {
+
+        public int AllProductsCount { get; set; }
+
+        public IEnumerable<ProductDetails> Products { get; set; }
+    }
+}
