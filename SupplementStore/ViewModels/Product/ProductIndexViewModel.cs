02341870687a333f0dad563613f5ba2f6b5ade2d
Adrian <mitura55@interia.pl>
2022-02-02 14:04:43 +0100

    Create ProductIndexViewModel.

---
+++ SupplementStore/ViewModels/Product/ProductIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SupplementStore.Application.Models;
+using System.Collections.Generic;
+
+namespace SupplementStore.ViewModels.Product {
+
+    public class ProductIndexViewModel {
+
+        public int Skip { get; set; }
+
+        public int Take { get; set; } = 10;
+
+        public int AllProductsCount { get; set; }
+
+        public IEnumerable<ProductDetails> Products { get; set; }
+    }
+}
