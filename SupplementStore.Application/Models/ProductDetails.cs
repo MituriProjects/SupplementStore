eec2fffc3803de3b00e5007bcb079ed5f13f93fc
Adrian <mitura55@interia.pl>
2022-02-02 14:02:21 +0100

    Create ProductDetails.

---
+++ SupplementStore.Application/Models/ProductDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace SupplementStore.Application.Models {
+
+    public class ProductDetails {
+
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
