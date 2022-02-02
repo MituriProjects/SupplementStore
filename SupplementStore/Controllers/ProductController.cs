2476a8b991b04b8be2dab7c5101235aa79352387
Adrian <mitura55@interia.pl>
2022-02-02 14:07:34 +0100

    Create a Product's Index action.

---
+++ SupplementStore/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SupplementStore.Application.Args;
+using SupplementStore.Application.Services;
+using SupplementStore.ViewModels.Product;
+
+namespace SupplementStore.Controllers {
+
+    public class ProductController : Controller {
+
+        IProductProvider ProductProvider { get; }
+
+        public ProductController(IProductProvider productProvider) {
+
+            ProductProvider = productProvider;
+        }
+
+        public IActionResult Index(ProductIndexViewModel model) {
+
+            var loadedProducts = ProductProvider.Load(new ProductProviderArgs {
+                Skip = model.Skip,
+                Take = model.Take
+            });
+
+            model.AllProductsCount = loadedProducts.AllProductsCount;
+            model.Products = loadedProducts.Products;
+
+            return View(model);
+        }
+    }
+}
