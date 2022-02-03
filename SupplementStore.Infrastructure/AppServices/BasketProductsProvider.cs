4d6da4afb65200b83f9abb7d60848ac3bfaebf72
Adrian <mitura55@interia.pl>
2022-02-03 20:41:34 +0100

    Create BasketProductsProvider.

---
+++ SupplementStore.Infrastructure/AppServices/BasketProductsProvider.cs
@@ -0,0 +1,39 @@
+﻿using SupplementStore.Application.Models;
+using SupplementStore.Application.Services;
+using SupplementStore.Domain.Entities.Baskets;
+using SupplementStore.Domain.Entities.Products;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SupplementStore.Infrastructure.AppServices {
+
+    public class BasketProductsProvider : IBasketProductsProvider {
+
+        IDocument<Product> ProductDocument { get; }
+
+        IDocument<BasketProduct> BasketProductDocument { get; }
+
+        public BasketProductsProvider(
+            IDocument<Product> productDocument,
+            IDocument<BasketProduct> basketProductDocument) {
+
+            ProductDocument = productDocument;
+            BasketProductDocument = basketProductDocument;
+        }
+
+        public IEnumerable<BasketProductDetails> Load(string userId) {
+
+            foreach (var basketProduct in BasketProductDocument.All.Where(e => e.UserId == userId).ToList()) {
+
+                var product = ProductDocument.All.First(e => e.Id == basketProduct.ProductId);
+
+                yield return new BasketProductDetails {
+                    ProductId = basketProduct.ProductId.ToString(),
+                    ProductName = product.Name,
+                    ProductPrice = product.Price,
+                    Quantity = basketProduct.Quantity
+                };
+            }
+        }
+    }
+}
