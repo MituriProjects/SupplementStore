a50505f26930196b9c285094a380743d6bc4f392
Adrian <mitura55@interia.pl>
2022-02-03 20:40:24 +0100

    Create BasketProductDetails.

---
+++ SupplementStore.Application/Models/BasketProductDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace SupplementStore.Application.Models {
+
+    public class BasketProductDetails {
+
+        public string ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal ProductPrice { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
