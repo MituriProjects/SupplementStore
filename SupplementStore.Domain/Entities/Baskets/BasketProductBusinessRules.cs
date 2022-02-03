e7db07830f7655e2e65903f399d3ea357b30c327
Adrian <mitura55@interia.pl>
2022-02-03 18:29:24 +0100

    Create BasketProductBusinessRules.

---
+++ SupplementStore.Domain/Entities/Baskets/BasketProductBusinessRules.cs
@@ -0,0 +1,8 @@
+﻿namespace SupplementStore.Domain.Entities.Baskets {
+
+    static class BasketProductBusinessRules {
+
+        public static readonly BusinessRule QuantityAboveZeroRequired = new BusinessRule(
+            "Quantity", "A basket product's quantity has to be above zero.");
+    }
+}
