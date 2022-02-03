d62710c2dc45d7fe8680b0b93262ec7cd290e3af
Adrian <mitura55@interia.pl>
2022-02-03 02:51:07 +0100

    Create IProductProvider.

---
+++ SupplementStore.Application/Services/IProductProvider.cs
@@ -0,0 +1,8 @@
+﻿using SupplementStore.Application.Models;
+
+namespace SupplementStore.Application.Services {
+
+    public interface IProductProvider {
+        ProductDetails Load(string productId);
+    }
+}
