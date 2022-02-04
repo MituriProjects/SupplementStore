4bf41e4c6f315ec868916c0659209e7370197ef4
Adrian <mitura55@interia.pl>
2022-02-04 02:45:03 +0100

    Create BasketProductRemover.

---
+++ SupplementStore.Infrastructure/AppServices/BasketProductRemover.cs
@@ -0,0 +1,34 @@
+﻿using SupplementStore.Application.Services;
+using SupplementStore.Domain.Entities.Baskets;
+using System;
+using System.Linq;
+
+namespace SupplementStore.Infrastructure.AppServices {
+
+    public class BasketProductRemover : IBasketProductRemover {
+
+        IDocument<BasketProduct> BasketProductDocument { get; }
+
+        IDocumentApprover DocumentApprover { get; }
+
+        public BasketProductRemover(
+            IDocument<BasketProduct> basketProductDocument,
+            IDocumentApprover documentApprover) {
+
+            BasketProductDocument = basketProductDocument;
+            DocumentApprover = documentApprover;
+        }
+
+        public void Remove(string userId, string productId) {
+
+            var basketProduct = BasketProductDocument.All.FirstOrDefault(e => e.UserId == userId && e.ProductId == Guid.Parse(productId));
+
+            if (basketProduct == null)
+                return;
+
+            BasketProductDocument.Delete(basketProduct.Id);
+
+            DocumentApprover.SaveChanges();
+        }
+    }
+}
