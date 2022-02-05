c55ee25502c6636ab28a2f7b3b4e250775f9ba51
Adrian <mitura55@interia.pl>
2022-02-05 18:33:47 +0100

    Create IBasketProductUpdater.

---
+++ SupplementStore.Application/Services/IBasketProductUpdater.cs
@@ -0,0 +1,8 @@
+﻿using SupplementStore.Application.Models;
+
+namespace SupplementStore.Application.Services {
+
+    public interface IBasketProductUpdater {
+        void Update(BasketProductDetails basketProductDetails);
+    }
+}
