a6409b801e49662d2f214b37a103cd3dd8bc9d41
Adrian <mitura55@interia.pl>
2022-02-05 20:08:04 +0100

    Create the BasketProductApi's Get action.

---
+++ SupplementStore/Controllers/BasketProductApiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SupplementStore.Application.Models;
+using SupplementStore.Application.Services;
+
+namespace SupplementStore.Controllers {
+
+    [Route("api/basketproduct")]
+    public class BasketProductApiController : Controller {
+
+        IBasketProductProvider BasketProductProvider { get; }
+
+        public BasketProductApiController(
+            IBasketProductProvider basketProductProvider) {
+
+            BasketProductProvider = basketProductProvider;
+        }
+
+        [HttpGet("{id}")]
+        public BasketProductDetails Get(string id) {
+
+            return BasketProductProvider.Load(id);
+        }
+    }
+}
