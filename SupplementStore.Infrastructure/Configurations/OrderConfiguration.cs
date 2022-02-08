c25b01978dfac51d4d947ae1eaf17b4aa119e0e0
Adrian <mitura55@interia.pl>
2022-02-08 02:04:14 +0100

    Create OrderConfiguration.

---
+++ SupplementStore.Infrastructure/Configurations/OrderConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SupplementStore.Domain.Entities.Orders;
+
+namespace SupplementStore.Infrastructure.Configurations {
+
+    class OrderConfiguration : EntityConfiguration<Order> {
+
+        protected override void ConfigureEntity(EntityTypeBuilder<Order> builder) {
+
+            builder.HasOne<IdentityUser>()
+                .WithMany()
+                .HasForeignKey(e => e.UserId)
+                .IsRequired();
+            builder.Property(e => e.Address)
+                .IsRequired();
+            builder.Property(e => e.PostalCode)
+                .IsRequired();
+            builder.Property(e => e.City)
+                .IsRequired();
+        }
+    }
+}
