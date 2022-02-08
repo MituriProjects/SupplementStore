d4b0ed95146e37b87dcfada7d5fb870722b6d380
Adrian <mitura55@interia.pl>
2022-02-08 02:07:13 +0100

    Create OrderProductBusinessRules.

---
+++ SupplementStore.Domain/Entities/Orders/OrderProductBusinessRules.cs
@@ -0,0 +1,8 @@
+﻿namespace SupplementStore.Domain.Entities.Orders {
+
+    static class OrderProductBusinessRules {
+
+        public static readonly BusinessRule QuantityAboveZeroRequired = new BusinessRule(
+            "Quantity", "An order product's quantity has to be above zero.");
+    }
+}
