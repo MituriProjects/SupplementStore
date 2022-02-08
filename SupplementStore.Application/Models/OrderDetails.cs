adb0ce51ee6cb47b3d84a11d60dec5715537b1eb
Adrian <mitura55@interia.pl>
2022-02-08 04:17:43 +0100

    Create OrderDetails.

--- SupplementStore.Application/Models/OrderDetails.cs
+++ SupplementStore.Application/Models/OrderDetails.cs
@@ -1,7 +1,22 @@
-﻿namespace SupplementStore.Application.Models {
+﻿using System;
+using System.Collections.Generic;
+
+namespace SupplementStore.Application.Models {
 
     public class OrderDetails {
 
         public string Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Address { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string City { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public IEnumerable<OrderProductDetails> OrderProducts { get; set; }
     }
 }
