8aa5f09d2a4d86acde077d017d05a4371b462fe6
Adrian <mitura55@interia.pl>
2022-02-08 01:55:39 +0100

    Create OrderBusinessRules.

---
+++ SupplementStore.Domain/Entities/Orders/OrderBusinessRules.cs
@@ -0,0 +1,8 @@
+﻿namespace SupplementStore.Domain.Entities.Orders {
+
+    static class OrderBusinessRules {
+
+        public static readonly BusinessRule PostalCodeInvalidFormat = new BusinessRule(
+            "PostalCode", "An order's postal code has invalid format.");
+    }
+}
