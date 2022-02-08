db46a50f12dd3cb219c9346e001d82127940b64f
Adrian <mitura55@interia.pl>
2022-02-08 15:11:20 +0100

    Remove static helper methods from TestOrderProduct.

--- SupplementStore.Tests/TestOrderProduct.cs
+++ SupplementStore.Tests/TestOrderProduct.cs
@@ -1,6 +1,5 @@
 ﻿using SupplementStore.Domain.Entities.Orders;
 using System;
-using System.Linq;
 
 namespace SupplementStore.Tests {
 
@@ -26,33 +25,5 @@ namespace SupplementStore.Tests {
 
             return this;
         }
-
-        public static TestOrderProduct New() {
-
-            var orderProduct = new TestOrderProduct();
-
-            TestDocument<OrderProduct>.Add(orderProduct);
-
-            return orderProduct;
-        }
-
-        public static TestOrderProduct Random() {
-
-            var id = Guid.NewGuid();
-
-            var orderProduct = new TestOrderProduct {
-                Id = id,
-                OrderId = Guid.NewGuid(),
-                ProductId = Guid.NewGuid(),
-                Quantity = RandomManager.Next(10)
-            };
-
-            TestDocument<OrderProduct>.Add(orderProduct);
-
-            return orderProduct;
-        }
-
-        public static TestOrderProduct[] Random(int count) =>
-            new TestOrderProduct[count].Select(e => Random()).ToArray();
     }
 }
