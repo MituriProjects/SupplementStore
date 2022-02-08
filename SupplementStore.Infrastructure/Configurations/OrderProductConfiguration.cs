0675eaa7f059e2b51a6297077d0ee32ae2a5c52d
Adrian <mitura55@interia.pl>
2022-02-08 02:07:30 +0100

    Create OrderProductConfiguration.

---
+++ SupplementStore.Infrastructure/Configurations/OrderProductConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SupplementStore.Domain.Entities.Orders;
+using SupplementStore.Domain.Entities.Products;
+
+namespace SupplementStore.Infrastructure.Configurations {
+
+    class OrderProductConfiguration : EntityConfiguration<OrderProduct> {
+
+        protected override void ConfigureEntity(EntityTypeBuilder<OrderProduct> builder) {
+
+            builder.HasOne<Order>()
+                .WithMany()
+                .HasForeignKey(e => e.OrderId)
+                .IsRequired();
+            builder.HasOne<Product>()
+                .WithMany()
+                .HasForeignKey(e => e.ProductId)
+                .IsRequired();
+        }
+    }
+}
