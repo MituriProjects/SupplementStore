7f860d943e734ee006efd2827df053ecdb181224
Adrian <mitura55@interia.pl>
2022-02-08 02:18:20 +0100

    Create OrderCreatorArgs.

---
+++ SupplementStore.Application/Args/OrderCreatorArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace SupplementStore.Application.Args {
+
+    public class OrderCreatorArgs {
+
+        public string UserId { get; set; }
+
+        public string Address { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string City { get; set; }
+    }
+}
