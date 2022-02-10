6b25ce918ec384a83d54d371396cd91e819a3534
Adrian <mitura55@interia.pl>
2022-02-10 12:45:15 +0100

    Create Address.

---
+++ SupplementStore.Domain/Entities/Orders/Address.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace SupplementStore.Domain.Entities.Orders {
+
+    public class Address : ValueObject<Address> {
+
+        public string Street { get; private set; }
+
+        public string PostalCode { get; private set; }
+
+        public string City { get; private set; }
+
+        private Address() {
+        }
+
+        public Address(string street, string postalCode, string city) {
+
+            Street = street;
+            PostalCode = postalCode;
+            City = city;
+        }
+
+        protected override IEnumerable<object> GetValues() {
+
+            return new object[] {
+                Street,
+                PostalCode,
+                City
+            };
+        }
+    }
+}
