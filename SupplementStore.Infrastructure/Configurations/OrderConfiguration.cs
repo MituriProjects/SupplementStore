0a9d732c18ff2e6ed2f12b1c9b8875b5c411b820
Adrian <mitura55@interia.pl>
2022-02-10 12:50:35 +0100

    Rework OrderConfiguration.

--- SupplementStore.Infrastructure/Configurations/OrderConfiguration.cs
+++ SupplementStore.Infrastructure/Configurations/OrderConfiguration.cs
@@ -12,11 +12,14 @@ namespace SupplementStore.Infrastructure.Configurations {
                 .WithMany()
                 .HasForeignKey(e => e.UserId)
                 .IsRequired();
-            builder.Property(e => e.Address)
+            builder.OwnsOne(e => e.Address)
+                .Property(e => e.Street)
                 .IsRequired();
-            builder.Property(e => e.PostalCode)
+            builder.OwnsOne(e => e.Address)
+                .Property(e => e.PostalCode)
                 .IsRequired();
-            builder.Property(e => e.City)
+            builder.OwnsOne(e => e.Address)
+                .Property(e => e.City)
                 .IsRequired();
         }
     }
