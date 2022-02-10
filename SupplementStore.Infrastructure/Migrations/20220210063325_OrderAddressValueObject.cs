5a9ccf02c6ed2c74ae836e4b22308cc1273615b4
Adrian <mitura55@interia.pl>
2022-02-10 12:51:04 +0100

    Create an OrderAddressValueObject migration.

---
+++ SupplementStore.Infrastructure/Migrations/20220210063325_OrderAddressValueObject.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SupplementStore.Infrastructure.Migrations
+{
+    public partial class OrderAddressValueObject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PostalCode",
+                table: "Orders",
+                newName: "Address_PostalCode");
+
+            migrationBuilder.RenameColumn(
+                name: "City",
+                table: "Orders",
+                newName: "Address_City");
+
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "Orders",
+                newName: "Address_Street");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Address_PostalCode",
+                table: "Orders",
+                newName: "PostalCode");
+
+            migrationBuilder.RenameColumn(
+                name: "Address_City",
+                table: "Orders",
+                newName: "City");
+
+            migrationBuilder.RenameColumn(
+                name: "Address_Street",
+                table: "Orders",
+                newName: "Address");
+        }
+    }
+}
