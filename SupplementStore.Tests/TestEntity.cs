dc8a0d5fddd0dffb57f7f24e855d2e2fdb98f3e5
Adrian <mitura55@interia.pl>
2022-02-10 13:13:30 +0100

    Modify TestEntity not to operate on test entities.

--- SupplementStore.Tests/TestEntity.cs
+++ SupplementStore.Tests/TestEntity.cs
@@ -8,37 +8,37 @@ namespace SupplementStore.Tests {
 
     static class TestEntity {
 
-        public static TTestEntity Random<TTestEntity>() {
+        public static TEntity Random<TEntity>() {
 
-            var testEntity = Create<TTestEntity>();
+            var entity = Create<TEntity>();
 
-            AssignId(testEntity);
-            AssignPropertyValues(testEntity);
-            AddToDocument(testEntity);
+            AssignId(entity);
+            AssignPropertyValues(entity);
+            AddToDocument(entity);
 
-            return testEntity;
+            return entity;
         }
 
-        public static TTestEntity[] Random<TTestEntity>(int count) {
+        public static TEntity[] Random<TEntity>(int count) {
 
-            return new TTestEntity[count]
-                .Select(e => Random<TTestEntity>())
+            return new TEntity[count]
+                .Select(e => Random<TEntity>())
                 .ToArray();
         }
 
-        private static TTestEntity Create<TTestEntity>() {
+        private static TEntity Create<TEntity>() {
 
-            return Activator.CreateInstance<TTestEntity>();
+            return Activator.CreateInstance<TEntity>();
         }
 
-        private static void AssignId(object testEntity) {
+        private static void AssignId(object entity) {
 
             var id = Guid.NewGuid();
 
-            testEntity.GetType().GetProperty("Id").SetValue(testEntity, id);
+            entity.GetType().GetProperty("Id").SetValue(entity, id);
         }
 
-        private static void AssignPropertyValues(object testEntity) {
+        private static void AssignPropertyValues(object entity) {
 
             var propertyActions = new Dictionary<Type, object> {
                 { typeof(Guid), Guid.NewGuid() },
@@ -46,21 +46,21 @@ namespace SupplementStore.Tests {
                 { typeof(decimal), RandomManager.Next(10000) / 100M }
             };
 
-            foreach (var property in testEntity.GetType().GetProperties()) {
+            foreach (var property in entity.GetType().GetProperties()) {
 
                 if (property.Name == "Id")
                     continue;
 
                 if (property.Name == "UserId") {
 
-                    property.SetValue(testEntity, Guid.NewGuid().ToString());
+                    property.SetValue(entity, Guid.NewGuid().ToString());
 
                     continue;
                 }
 
                 if (property.PropertyType == typeof(string)) {
 
-                    property.SetValue(testEntity, $"{testEntity.GetType().Name}-{property.Name}-{testEntity.GetType().GetProperty("Id").GetValue(testEntity)}");
+                    property.SetValue(entity, $"{entity.GetType().Name}-{property.Name}-{entity.GetType().GetProperty("Id").GetValue(entity)}");
 
                     continue;
                 }
@@ -76,29 +76,27 @@ namespace SupplementStore.Tests {
 
                     foreach (var parameter in parameters) {
 
-                        parameterValues.Add($"{testEntity.GetType().Name}-{property.Name}-{parameter.Name[0].ToString().ToUpper() + parameter.Name.Substring(1)}-{testEntity.GetType().GetProperty("Id").GetValue(testEntity)}");
+                        parameterValues.Add($"{entity.GetType().Name}-{property.Name}-{parameter.Name[0].ToString().ToUpper() + parameter.Name.Substring(1)}-{entity.GetType().GetProperty("Id").GetValue(entity)}");
                     }
 
                     var obj = constructor.Invoke(parameterValues.ToArray());
 
-                    property.SetValue(testEntity, obj);
+                    property.SetValue(entity, obj);
 
                     continue;
                 }
 
                 if (propertyActions.ContainsKey(property.PropertyType))
-                    property.SetValue(testEntity, propertyActions[property.PropertyType]);
+                    property.SetValue(entity, propertyActions[property.PropertyType]);
             }
         }
 
-        private static void AddToDocument(object testEntity) {
-
-            var baseType = testEntity.GetType().BaseType;
+        private static void AddToDocument(object entity) {
 
             typeof(TestDocument<>)
-                .MakeGenericType(baseType)
+                .MakeGenericType(entity.GetType())
                 .GetMethod("Add")
-                .Invoke(null, new object[] { testEntity });
+                .Invoke(null, new object[] { entity });
         }
     }
 }
