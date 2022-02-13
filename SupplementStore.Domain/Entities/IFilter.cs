07c82943dd5df8e0eec2a713aaf064cbbfb5ebd4
Adrian <mitura55@interia.pl>
2022-02-13 17:13:40 +0100

    Create IFilter.

---
+++ SupplementStore.Domain/Entities/IFilter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SupplementStore.Domain.Entities {
+
+    public interface IFilter<TEntity>
+        where TEntity : Entity {
+
+        TEntity Process(IEnumerable<TEntity> entities);
+    }
+}
