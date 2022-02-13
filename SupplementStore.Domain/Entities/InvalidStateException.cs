8f8d4c14fcc678fb19401fdcd140bb92f91afe2c
Adrian <mitura55@interia.pl>
2022-02-13 13:41:00 +0100

    Create InvalidStateException.

---
+++ SupplementStore.Domain/Entities/InvalidStateException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SupplementStore.Domain.Entities {
+
+    [Serializable]
+    public class InvalidStateException : Exception {
+
+        public InvalidStateException() {
+        }
+
+        public InvalidStateException(string message) : base(message) {
+        }
+
+        public InvalidStateException(string message, Exception innerException) : base(message, innerException) {
+        }
+
+        protected InvalidStateException(SerializationInfo info, StreamingContext context) : base(info, context) {
+        }
+    }
+}
