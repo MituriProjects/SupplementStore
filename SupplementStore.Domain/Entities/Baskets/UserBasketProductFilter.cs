756c028f44a4ec00b8a949c543cbf271228a51fd
Adrian <mitura55@interia.pl>
2022-02-13 17:24:03 +0100

    Create UserBasketProductFilter.

---
+++ SupplementStore.Domain/Entities/Baskets/UserBasketProductFilter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SupplementStore.Domain.Entities.Baskets {
+
+    public class UserBasketProductFilter : IFilter<BasketProduct> {
+
+        string UserId { get; }
+
+        Guid ProductId { get; }
+
+        public UserBasketProductFilter(string userId, Guid productId) {
+
+            UserId = userId;
+            ProductId = productId;
+        }
+
+        public BasketProduct Process(IEnumerable<BasketProduct> entities) {
+
+            return entities.FirstOrDefault(e => e.UserId == UserId && e.ProductId == ProductId);
+        }
+    }
+}
