b80d2ad4f4aa732f923f31dd1c8d49009a73e09a
Adrian <mitura55@interia.pl>
2022-02-13 13:39:27 +0100

    Create AddressFactory.

---
+++ SupplementStore.Tests/AddressFactory.cs
@@ -0,0 +1,17 @@
+﻿using SupplementStore.Domain.Entities;
+using SupplementStore.Domain.Entities.Orders;
+
+namespace SupplementStore.Tests {
+
+    static class AddressFactory {
+
+        public static Address Create(Entity entity) {
+
+            var street = $"{entity.GetType().Name}-Address-Street-{entity.GetType().GetProperty("Id").GetValue(entity)}";
+            var postalCode = $"{RandomManager.Next(100).ToString("00")}-{RandomManager.Next(1000).ToString("000")}";
+            var city = $"{entity.GetType().Name}-Address-City-{entity.GetType().GetProperty("Id").GetValue(entity)}";
+
+            return new Address(street, postalCode, city);
+        }
+    }
+}
