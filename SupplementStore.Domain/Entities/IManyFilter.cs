d3cac6eac79d59f259b592914839483715e86fb1
Adrian <mitura55@interia.pl>
2022-02-13 17:13:55 +0100

    Create IManyFilter.

---
+++ SupplementStore.Domain/Entities/IManyFilter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SupplementStore.Domain.Entities {
+
+    public interface IManyFilter<TEntity>
+        where TEntity : Entity {
+
+        IEnumerable<TEntity> Process(IEnumerable<TEntity> entities);
+    }
+}
