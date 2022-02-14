c5ecd858d887c25b9c5c97f014eb70f47f4532eb
Adrian <mitura55@interia.pl>
2022-02-14 08:57:25 +0100

    Modify OrderProduct to use Quantity.

--- SupplementStore.Domain/Entities/Orders/OrderProduct.cs
+++ SupplementStore.Domain/Entities/Orders/OrderProduct.cs
@@ -1,19 +1,22 @@
-﻿using System;
+﻿using SupplementStore.Domain.Entities.Shared;
+using System;
 
 namespace SupplementStore.Domain.Entities.Orders {
 
     public class OrderProduct : Entity {
 
+        Quantity ProductQuantity { get; set; }
+
         public Guid OrderId { get; set; }
 
         public Guid ProductId { get; set; }
 
-        public int Quantity { get; set; }
+        public int Quantity {
+            get => ProductQuantity.Value;
+            set => ProductQuantity = new Quantity(value);
+        }
 
         protected override void Validate() {
-
-            if (Quantity <= 0)
-                AddBrokenRule(OrderProductBusinessRules.QuantityAboveZeroRequired);
         }
     }
 }
