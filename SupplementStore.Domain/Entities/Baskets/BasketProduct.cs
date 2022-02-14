c4bad0cf358ccd98ff3c852cfd72a031b8843b7f
Adrian <mitura55@interia.pl>
2022-02-14 08:49:13 +0100

    Modify BasketProduct to use Quantity.

--- SupplementStore.Domain/Entities/Baskets/BasketProduct.cs
+++ SupplementStore.Domain/Entities/Baskets/BasketProduct.cs
@@ -4,16 +4,18 @@ namespace SupplementStore.Domain.Entities.Baskets {
 
     public class BasketProduct : Entity {
 
+        Quantity ProductQuantity { get; set; }
+
         public string UserId { get; set; }
 
         public Guid ProductId { get; set; }
 
-        public int Quantity { get; set; }
+        public int Quantity {
+            get => ProductQuantity.Value;
+            set => ProductQuantity = new Quantity(value);
+        }
 
         protected override void Validate() {
-
-            if (Quantity <= 0)
-                AddBrokenRule(BasketProductBusinessRules.QuantityAboveZeroRequired);
         }
     }
 }
