c4459b909a18721fa848ae7f41b10c6752a89714
Adrian <mitura55@interia.pl>
2022-02-14 09:18:51 +0100

    Delete OrderBusinessRules.

--- SupplementStore.Domain/Entities/Orders/OrderBusinessRules.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SupplementStore.Domain.Entities.Orders {
-
-    static class OrderBusinessRules {
-
-        public static readonly BusinessRule PostalCodeInvalidFormat = new BusinessRule(
-            "PostalCode", "An order's postal code has invalid format.");
-    }
-}
