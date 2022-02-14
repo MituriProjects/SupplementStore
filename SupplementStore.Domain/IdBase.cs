772d986cad99a5d38c6a35a13a73ca389ec3da7a
Adrian <mitura55@interia.pl>
2022-02-14 20:45:13 +0100

    Create IdBase.

---
+++ SupplementStore.Domain/IdBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SupplementStore.Domain {
+
+    public abstract class IdBase : ValueObject<IdBase> {
+
+        Guid Id { get; set; }
+
+        protected IdBase(Guid id) {
+
+            Id = id;
+        }
+
+        protected override IEnumerable<object> GetValues() {
+
+            return new object[] {
+                Id
+            };
+        }
+    }
+}
