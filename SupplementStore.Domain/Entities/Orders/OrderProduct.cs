d5174c32b0ba3d8d62f20d97e7a60a089d87c9f0
Adrian <mitura55@interia.pl>
2022-02-14 09:15:44 +0100

    Remove the Validate method from OrderProduct.

--- SupplementStore.Domain/Entities/Orders/OrderProduct.cs
+++ SupplementStore.Domain/Entities/Orders/OrderProduct.cs
@@ -15,8 +15,5 @@ namespace SupplementStore.Domain.Entities.Orders {
             get => ProductQuantity.Value;
             set => ProductQuantity = new Quantity(value);
         }
-
-        protected override void Validate() {
-        }
     }
 }
