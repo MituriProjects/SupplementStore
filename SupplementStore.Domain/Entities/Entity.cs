d2255b44f3e455726de446ed7fc4e003f1217d83
Adrian <mitura55@interia.pl>
2022-02-14 09:00:08 +0100

    Remove the broken rules management from Entity.

--- SupplementStore.Domain/Entities/Entity.cs
+++ SupplementStore.Domain/Entities/Entity.cs
@@ -1,29 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace SupplementStore.Domain.Entities {
 
     public abstract class Entity {
 
         public Guid Id { get; set; }
-
-        private List<BusinessRule> BrokenRules { get; } = new List<BusinessRule>();
-
-        public IEnumerable<BusinessRule> GetBrokenRules() {
-
-            BrokenRules.Clear();
-
-            Validate();
-
-            return BrokenRules;
-        }
-
-        protected void AddBrokenRule(BusinessRule businessRule) {
-
-            BrokenRules.Add(businessRule);
-        }
-
-
-        protected abstract void Validate();
     }
 }
