19d373bbef83d3ce50f4068dd170c1644e9769bb
Adrian <mitura55@interia.pl>
2022-02-14 20:50:28 +0100

    Create BasketProductId.

---
+++ SupplementStore.Domain/Baskets/BasketProductId.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SupplementStore.Domain.Baskets {
+
+    public class BasketProductId : IdBase {
+
+        public BasketProductId(Guid id) : base(id) {
+        }
+    }
+}
