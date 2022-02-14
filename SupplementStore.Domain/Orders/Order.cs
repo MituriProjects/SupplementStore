f357d107b9ca40a03b30e25c9c1ceb5567aeb9d7
Adrian <mitura55@interia.pl>
2022-02-14 20:49:24 +0100

    Modify Order to use OrderId.

--- SupplementStore.Domain/Orders/Order.cs
+++ SupplementStore.Domain/Orders/Order.cs
@@ -4,6 +4,8 @@ namespace SupplementStore.Domain.Orders {
 
     public class Order : Entity {
 
+        public OrderId OrderId { get; private set; } = new OrderId(Guid.Empty);
+
         public string UserId { get; set; }
 
         public Address Address { get; set; }
