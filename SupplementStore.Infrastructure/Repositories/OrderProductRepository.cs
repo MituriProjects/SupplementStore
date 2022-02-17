d02225b27bd493ed99b2870022996117ccc851f4
Adrian <mitura55@interia.pl>
2022-02-17 18:18:26 +0100

    Create OrderProductRepository.

---
+++ SupplementStore.Infrastructure/Repositories/OrderProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using SupplementStore.Domain.Orders;
+using System.Linq;
+
+namespace SupplementStore.Infrastructure.Repositories {
+
+    public class OrderProductRepository : Repository<OrderProduct>, IOrderProductRepository {
+
+        public OrderProductRepository(IDocument<OrderProduct> document) : base(document) {
+        }
+
+        public OrderProduct FindBy(OrderProductId orderProductId) {
+
+            return Document.All
+                .FirstOrDefault(e => e.OrderProductId == orderProductId);
+        }
+    }
+}
