23c077c9b4fe3e179e318899d6b6fa4aa4eadc47
Adrian <mitura55@interia.pl>
2022-02-18 08:31:29 +0100

    Add a parameterless constructor to OrderId.

--- SupplementStore.Domain/Orders/OrderId.cs
+++ SupplementStore.Domain/Orders/OrderId.cs
@@ -4,6 +4,9 @@ namespace SupplementStore.Domain.Orders {
 
     public class OrderId : IdBase {
 
+        public OrderId() : base("") {
+        }
+
         public OrderId(Guid id) : base(id) {
         }
     }
