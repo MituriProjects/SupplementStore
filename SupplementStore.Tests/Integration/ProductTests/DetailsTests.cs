14d89c058216a62ee11e683500c12931eea01cb3
Adrian <mitura55@interia.pl>
2022-02-18 10:18:20 +0100

    Modify Product's Details tests not to use the Entity's Id property.

--- SupplementStore.Tests/Integration/ProductTests/DetailsTests.cs
+++ SupplementStore.Tests/Integration/ProductTests/DetailsTests.cs
@@ -13,10 +13,10 @@ namespace SupplementStore.Tests.Integration.ProductTests {
 
             var products = TestEntity.Random<Product>(3);
 
-            await GetAsync($"/Product/Details/{products[1].Id}");
+            await GetAsync($"/Product/Details/{products[1].ProductId}");
 
             Examine(ContentScheme.Html()
-                .Contains("ProductId", products[1].Id)
+                .Contains("ProductId", products[1].ProductId)
                 .Contains("ProductName", products[1].Name)
                 .Contains("ProductPrice", products[1].Price));
         }
