c5064d23d70decb993d350d5d620ada51da63126
Adrian <mitura55@interia.pl>
2022-02-18 08:52:37 +0100

    Create BasketProductRepository.

---
+++ SupplementStore.Infrastructure/Repositories/BasketProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using SupplementStore.Domain.Baskets;
+using System.Linq;
+
+namespace SupplementStore.Infrastructure.Repositories {
+
+    public class BasketProductRepository : Repository<BasketProduct>, IBasketProductRepository {
+
+        public BasketProductRepository(IDocument<BasketProduct> document) : base(document) {
+        }
+
+        public BasketProduct FindBy(BasketProductId basketProductId) {
+
+            return Document.All
+                .FirstOrDefault(e => e.BasketProductId == basketProductId);
+        }
+    }
+}
