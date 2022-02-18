6948eddc377f380ef74fcdc8f07ea4750eb17bf5
Adrian <mitura55@interia.pl>
2022-02-18 09:13:06 +0100

    Remove the parameterless constructor from BasketProductId.

--- SupplementStore.Domain/Baskets/BasketProductId.cs
+++ SupplementStore.Domain/Baskets/BasketProductId.cs
@@ -4,9 +4,6 @@ namespace SupplementStore.Domain.Baskets {
 
     public class BasketProductId : IdBase {
 
-        public BasketProductId() : base("") {
-        }
-
         public BasketProductId(Guid id) : base(id) {
         }
 
