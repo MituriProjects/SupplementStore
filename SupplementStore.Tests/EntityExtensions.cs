95022c7ff077e2ac13a9468dc015e384b4342848
Adrian <mitura55@interia.pl>
2022-02-18 10:45:22 +0100

    Create Entity's GetId and SetId extension methods.

---
+++ SupplementStore.Tests/EntityExtensions.cs
@@ -0,0 +1,23 @@
+﻿using SupplementStore.Domain;
+using System;
+using System.Reflection;
+
+namespace SupplementStore.Tests {
+
+    static class EntityExtensions {
+
+        public static object GetId(this Entity entity) {
+
+            return typeof(Entity)
+                .GetProperty("Id", BindingFlags.NonPublic | BindingFlags.Instance)
+                .GetValue(entity);
+        }
+
+        public static void SetId(this Entity entity, Guid id) {
+
+            typeof(Entity)
+                .GetProperty("Id", BindingFlags.NonPublic | BindingFlags.Instance)
+                .SetValue(entity, id);
+        }
+    }
+}
