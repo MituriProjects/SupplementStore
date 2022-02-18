7caa1296d6b0fc7bc2b862073132771d3cef0013
Adrian <mitura55@interia.pl>
2022-02-18 07:59:59 +0100

    Create an OrderProduct_OrderId migration.

---
+++ SupplementStore.Infrastructure/Migrations/20220218065729_OrderProduct_OrderId.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SupplementStore.Infrastructure.Migrations
+{
+    public partial class OrderProduct_OrderId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProducts_Orders_OrderId",
+                table: "OrderProducts");
+
+            migrationBuilder.RenameColumn(
+                name: "OrderId",
+                table: "OrderProducts",
+                newName: "Order_Id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderProducts_OrderId",
+                table: "OrderProducts",
+                newName: "IX_OrderProducts_Order_Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProducts_Orders_Order_Id",
+                table: "OrderProducts",
+                column: "Order_Id",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProducts_Orders_Order_Id",
+                table: "OrderProducts");
+
+            migrationBuilder.RenameColumn(
+                name: "Order_Id",
+                table: "OrderProducts",
+                newName: "OrderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderProducts_Order_Id",
+                table: "OrderProducts",
+                newName: "IX_OrderProducts_OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProducts_Orders_OrderId",
+                table: "OrderProducts",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
