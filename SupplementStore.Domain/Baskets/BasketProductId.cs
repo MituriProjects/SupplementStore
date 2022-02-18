287a5d4b532628bd509901acadd3e7c48edea31f
Adrian <mitura55@interia.pl>
2022-02-18 09:00:53 +0100

    Add a parameterless constructor to BasketProductId.

--- SupplementStore.Domain/Baskets/BasketProductId.cs
+++ SupplementStore.Domain/Baskets/BasketProductId.cs
@@ -4,6 +4,9 @@ namespace SupplementStore.Domain.Baskets {
 
     public class BasketProductId : IdBase {
 
+        public BasketProductId() : base("") {
+        }
+
         public BasketProductId(Guid id) : base(id) {
         }
     }
