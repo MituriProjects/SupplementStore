41ce2a1af4a560841f9fc93ad78ec77239cf07dc
Adrian <mitura55@interia.pl>
2022-02-18 09:44:35 +0100

    Add another Delete method to IBasketProductRepository.

--- SupplementStore.Domain/Baskets/IBasketProductRepository.cs
+++ SupplementStore.Domain/Baskets/IBasketProductRepository.cs
@@ -1,6 +1,7 @@
 ﻿namespace SupplementStore.Domain.Baskets {
 
     public interface IBasketProductRepository : IRepository<BasketProduct> {
+        void Delete(BasketProductId basketProductId);
         BasketProduct FindBy(BasketProductId basketProductId);
     }
 }
