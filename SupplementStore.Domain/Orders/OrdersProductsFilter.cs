8cf892b3f0adf107602a3ee945cbf53ae697591c
Adrian <mitura55@interia.pl>
2022-02-21 12:35:37 +0100

    Create OrdersProductsFilter.

---
+++ SupplementStore.Domain/Orders/OrdersProductsFilter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SupplementStore.Domain.Orders {
+
+    public class OrdersProductsFilter : IManyFilter<OrderProduct> {
+
+        IEnumerable<OrderId> OrderIds { get; }
+
+        public OrdersProductsFilter(IEnumerable<OrderId> orderIds) {
+
+            OrderIds = orderIds;
+        }
+
+        public IEnumerable<OrderProduct> Process(IQueryable<OrderProduct> entities) {
+
+            return entities.Where(e => OrderIds.Contains(e.OrderId)).ToList();
+        }
+    }
+}
