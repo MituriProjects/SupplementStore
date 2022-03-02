9ca84366be3ab8fc1fb6be4cacdd67f7551057b7
Adrian <mitura55@interia.pl>
2022-03-02 07:42:44 +0100

    Create WishCreator.

---
+++ SupplementStore.Infrastructure/AppServices/WishCreator.cs
@@ -0,0 +1,45 @@
+﻿using SupplementStore.Application.Services;
+using SupplementStore.Domain.Products;
+using SupplementStore.Domain.Wishes;
+
+namespace SupplementStore.Infrastructure.AppServices {
+
+    public class WishCreator : IWishCreator {
+
+        IProductRepository ProductRepository { get; }
+
+        IWishRepository WishRepository { get; }
+
+        IDocumentApprover DocumentApprover { get; }
+
+        public WishCreator(
+            IProductRepository productRepository,
+            IWishRepository wishRepository,
+            IDocumentApprover documentApprover) {
+
+            ProductRepository = productRepository;
+            WishRepository = wishRepository;
+            DocumentApprover = documentApprover;
+        }
+
+        public void Create(string userId, string productId) {
+
+            var product = ProductRepository.FindBy(new ProductId(productId));
+
+            if (product == null)
+                return;
+
+            var wish = WishRepository.FindBy(new UserWishFilter(userId, new ProductId(productId)));
+
+            if (wish != null)
+                return;
+
+            WishRepository.Add(new Wish {
+                UserId = userId,
+                ProductId = new ProductId(productId)
+            });
+
+            DocumentApprover.SaveChanges();
+        }
+    }
+}
