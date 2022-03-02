773ce8c6bb4ecc18939d70297bc5abaef36c92bd
Adrian <mitura55@interia.pl>
2022-03-02 07:06:31 +0100

    Create WishId.

---
+++ SupplementStore.Domain/Wishes/WishId.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SupplementStore.Domain.Wishes {
+
+    public class WishId : IdBase {
+
+        public WishId(Guid id) : base(id) {
+        }
+
+        public WishId(string id) : base(id) {
+        }
+    }
+}
