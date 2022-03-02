4d5006512787dcab4e1ce8cfc0acf4b4b46432e1
Adrian <mitura55@interia.pl>
2022-03-02 07:43:38 +0100

    Create WishRemover.

---
+++ SupplementStore.Infrastructure/AppServices/WishRemover.cs
@@ -0,0 +1,28 @@
+﻿using SupplementStore.Application.Services;
+using SupplementStore.Domain.Products;
+using SupplementStore.Domain.Wishes;
+
+namespace SupplementStore.Infrastructure.AppServices {
+
+    public class WishRemover : IWishRemover {
+
+        IWishRepository WishRepository { get; }
+
+        IDocumentApprover DocumentApprover { get; }
+
+        public WishRemover(
+            IWishRepository wishRepository,
+            IDocumentApprover documentApprover) {
+
+            WishRepository = wishRepository;
+            DocumentApprover = documentApprover;
+        }
+
+        public void Remove(string userId, string productId) {
+
+            WishRepository.Delete(userId, new ProductId(productId));
+
+            DocumentApprover.SaveChanges();
+        }
+    }
+}
