243173c46b34356224f5fcaed8cd2de5e50449cb
Adrian <mitura55@interia.pl>
2022-03-02 07:08:32 +0100

    Create WishConfiguration.

---
+++ SupplementStore.Infrastructure/Configurations/WishConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SupplementStore.Domain.Products;
+using SupplementStore.Domain.Wishes;
+
+namespace SupplementStore.Infrastructure.Configurations {
+
+    class WishConfiguration : EntityConfiguration<Wish> {
+
+        protected override void ConfigureEntity(EntityTypeBuilder<Wish> builder) {
+
+            builder.HasOne<IdentityUser>()
+                .WithMany()
+                .HasForeignKey(e => e.UserId)
+                .IsRequired();
+            builder.Ignore(e => e.ProductId);
+            builder.HasOne<Product>()
+                .WithMany()
+                .HasForeignKey("Product_Id")
+                .IsRequired();
+        }
+    }
+}
