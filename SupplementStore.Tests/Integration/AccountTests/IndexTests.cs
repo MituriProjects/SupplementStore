bc35c36b72d607618281a616834a27a07d4350d9
Adrian <mitura55@interia.pl>
2022-03-03 04:21:03 +0100

    Create an Account's Index action.

---
+++ SupplementStore.Tests/Integration/AccountTests/IndexTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace SupplementStore.Tests.Integration.AccountTests {
+
+    [TestClass]
+    public class IndexTests : IntegrationTestsBase {
+
+        [TestMethod]
+        public async Task UserIsLoggedOut_RedirectsToLogin() {
+
+            await GetAsync("/Account");
+
+            ExamineAuthRedirect("/Account");
+        }
+    }
+}
