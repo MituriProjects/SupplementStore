d01fef935a4f6a25480ced50bb9aa7dd9c872016
Adrian <mitura55@interia.pl>
2022-03-09 14:31:22 +0100

    Create IProductToOpineProvider.

---
+++ SupplementStore.Application/Services/IProductToOpineProvider.cs
@@ -0,0 +1,8 @@
+﻿using SupplementStore.Application.Results;
+
+namespace SupplementStore.Application.Services {
+
+    public interface IProductToOpineProvider {
+        ProductToOpineResult Load(string userId);
+    }
+}
