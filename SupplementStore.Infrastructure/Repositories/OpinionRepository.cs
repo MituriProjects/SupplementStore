e8ce1614efdf918d636d5096dcb514c1f5c0daf0
Adrian <mitura55@interia.pl>
2022-03-09 13:52:28 +0100

    Create OpinionRepository.

---
+++ SupplementStore.Infrastructure/Repositories/OpinionRepository.cs
@@ -0,0 +1,10 @@
+﻿using SupplementStore.Domain.Opinions;
+
+namespace SupplementStore.Infrastructure.Repositories {
+
+    public class OpinionRepository : Repository<Opinion>, IOpinionRepository {
+
+        public OpinionRepository(IDocument<Opinion> document) : base(document) {
+        }
+    }
+}
