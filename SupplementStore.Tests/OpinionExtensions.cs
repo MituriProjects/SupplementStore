0762ea20c122323a2e25e8fe0778ca6833f744f2
Adrian <mitura55@interia.pl>
2022-03-09 13:44:37 +0100

    Create OpinionExtensions.

---
+++ SupplementStore.Tests/OpinionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using SupplementStore.Domain.Opinions;
+using SupplementStore.Domain.Orders;
+
+namespace SupplementStore.Tests {
+
+    static class OpinionExtensions {
+
+        public static Opinion WithOrderProductId(this Opinion opinion, OrderProductId orderProductId) {
+
+            opinion.OrderProductId = orderProductId;
+
+            return opinion;
+        }
+
+        public static Opinion WithText(this Opinion opinion, string text) {
+
+            opinion.Text = text;
+
+            return opinion;
+        }
+
+        public static Opinion WithGrade(this Opinion opinion, Grade grade) {
+
+            opinion.Grade = grade;
+
+            return opinion;
+        }
+    }
+}
