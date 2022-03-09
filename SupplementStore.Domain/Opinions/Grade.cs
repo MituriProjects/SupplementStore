e9b27e2a4ccc63f01a600c94d6722f12743db228
Adrian <mitura55@interia.pl>
2022-03-09 13:43:32 +0100

    Create Grade.

---
+++ SupplementStore.Domain/Opinions/Grade.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace SupplementStore.Domain.Opinions {
+
+    public class Grade : ValueObject<Grade> {
+
+        public int Stars { get; private set; }
+
+        private Grade() {
+        }
+
+        public Grade(int stars) {
+
+            Stars = stars;
+
+            Validate();
+        }
+
+        protected override IEnumerable<object> GetValues() {
+
+            yield return Stars;
+        }
+
+        private void Validate() {
+
+
+        }
+    }
+}
