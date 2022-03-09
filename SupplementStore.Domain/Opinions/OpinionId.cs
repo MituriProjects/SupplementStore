47d65236e9084d865bf1fa1a996d029dda1147e6
Adrian <mitura55@interia.pl>
2022-03-09 13:43:20 +0100

    Create OpinionId.

---
+++ SupplementStore.Domain/Opinions/OpinionId.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SupplementStore.Domain.Opinions {
+
+    public class OpinionId : IdBase {
+
+        public OpinionId(Guid id) : base(id) {
+        }
+
+        public OpinionId(string id) : base(id) {
+        }
+    }
+}
