8f7e1db1e4b77005bf56a66dc24ee0762baa046f
Adrian <mitura55@interia.pl>
2022-03-09 14:33:00 +0100

    Add an IOpinionCreator dependency to DependencyResolver.

--- SupplementStore.DependencyResolving/DependencyResolver.cs
+++ SupplementStore.DependencyResolving/DependencyResolver.cs
@@ -44,6 +44,7 @@ namespace SupplementStore.DependencyResolving {
             services.AddTransient<IWishCreator, WishCreator>();
             services.AddTransient<IWishRemover, WishRemover>();
             services.AddTransient<IProductToOpineProvider, ProductToOpineProvider>();
+            services.AddTransient<IOpinionCreator, OpinionCreator>();
         }
     }
 }
