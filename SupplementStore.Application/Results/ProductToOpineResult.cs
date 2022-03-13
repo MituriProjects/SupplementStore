0fc7f3710be22cdfc1960a0a8ecf8a504ef51c18
Adrian <mitura55@interia.pl>
2022-03-13 18:24:01 +0100

    Add order and product details to ProductToOpineResult.

--- SupplementStore.Application/Results/ProductToOpineResult.cs
+++ SupplementStore.Application/Results/ProductToOpineResult.cs
@@ -1,7 +1,17 @@
-﻿namespace SupplementStore.Application.Results {
+﻿using System;
+
+namespace SupplementStore.Application.Results {
 
     public class ProductToOpineResult {
 
+        public bool IsEmpty { get; set; }
+
         public string OrderProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public DateTime BuyingDate { get; set; }
+
+        public static ProductToOpineResult Empty => new ProductToOpineResult { IsEmpty = true };
     }
 }
