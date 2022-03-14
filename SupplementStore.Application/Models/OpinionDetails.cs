c1a199fe7eeaf7389de9e1569a638219820a0152
Adrian <mitura55@interia.pl>
2022-03-14 09:27:22 +0100

    Create OpinionDetails.

---
+++ SupplementStore.Application/Models/OpinionDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SupplementStore.Application.Models {
+
+    public class OpinionDetails {
+
+        public string ProductName { get; set; }
+
+        public DateTime BuyingDate { get; set; }
+
+        public int Stars { get; set; }
+
+        public string Text { get; set; }
+    }
+}
