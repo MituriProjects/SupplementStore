34aed459775c65203200167643b8efbb89445819
Adrian <mitura55@interia.pl>
2022-03-14 11:52:48 +0100

    Create ProductOpinionDetails.

---
+++ SupplementStore.Application/Models/ProductOpinionDetails.cs
@@ -0,0 +1,9 @@
+﻿namespace SupplementStore.Application.Models {
+
+    public class ProductOpinionDetails {
+
+        public int Stars { get; set; }
+
+        public string Text { get; set; }
+    }
+}
