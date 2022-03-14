5b9adb5018758667dd14e1c495279fffb7a59d7f
Adrian <mitura55@interia.pl>
2022-03-14 09:29:05 +0100

    Add a FindBy method to IOpinionRepository.

--- SupplementStore.Domain/Opinions/IOpinionRepository.cs
+++ SupplementStore.Domain/Opinions/IOpinionRepository.cs
@@ -1,5 +1,8 @@
-﻿namespace SupplementStore.Domain.Opinions {
+﻿using System.Collections.Generic;
+
+namespace SupplementStore.Domain.Opinions {
 
     public interface IOpinionRepository : IRepository<Opinion> {
+        IEnumerable<Opinion> FindBy(IEnumerable<OpinionId> opinionIds);
     }
 }
