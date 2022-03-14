87fd821725d01c11056d62f662c6c9b8e8de05c2
Adrian <mitura55@interia.pl>
2022-03-14 09:29:30 +0100

    Implement the FindBy method in OpinionRepository.

--- SupplementStore.Infrastructure/Repositories/OpinionRepository.cs
+++ SupplementStore.Infrastructure/Repositories/OpinionRepository.cs
@@ -1,10 +1,19 @@
 ﻿using SupplementStore.Domain.Opinions;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SupplementStore.Infrastructure.Repositories {
 
     public class OpinionRepository : Repository<Opinion>, IOpinionRepository {
 
         public OpinionRepository(IDocument<Opinion> document) : base(document) {
+        }
+
+        public IEnumerable<Opinion> FindBy(IEnumerable<OpinionId> opinionIds) {
+
+            return Document.All
+                .Where(e => opinionIds.Contains(e.OpinionId))
+                .ToList();
         }
     }
 }
