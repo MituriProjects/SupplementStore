65be02a104e37decfc2e4248a7a603b699226d56
Adrian <mitura55@interia.pl>
2022-03-14 11:24:44 +0100

    Create ProductDetailsViewModel.

---
+++ SupplementStore/ViewModels/Product/ProductDetailsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using SupplementStore.Application.Models;
+
+namespace SupplementStore.ViewModels.Product {
+
+    public class ProductDetailsViewModel {
+
+        public ProductDetails Product { get; set; }
+    }
+}
