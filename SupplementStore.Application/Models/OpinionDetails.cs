7ebb235fe086ff4555f722ca450a3c46c69d14a5
Adrian <mitura55@interia.pl>
2022-03-18 06:37:21 +0100

    Add an Id property to OpinionDetails.

--- SupplementStore.Application/Models/OpinionDetails.cs
+++ SupplementStore.Application/Models/OpinionDetails.cs
@@ -4,6 +4,8 @@ namespace SupplementStore.Application.Models {
 
     public class OpinionDetails {
 
+        public string Id { get; set; }
+
         public string ProductName { get; set; }
 
         public DateTime BuyingDate { get; set; }
