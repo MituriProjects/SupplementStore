86d32f992cbe00c41c7bb5183139ddaae63e6ce5
Adrian <mitura55@interia.pl>
2022-03-18 06:33:54 +0100

    Overload the IOpinionRepository's FindBy method.

--- SupplementStore.Domain/Opinions/IOpinionRepository.cs
+++ SupplementStore.Domain/Opinions/IOpinionRepository.cs
@@ -3,6 +3,7 @@
 namespace SupplementStore.Domain.Opinions {
 
     public interface IOpinionRepository : IRepository<Opinion> {
+        Opinion FindBy(OpinionId opinionId);
         IEnumerable<Opinion> FindBy(IEnumerable<OpinionId> opinionIds);
     }
 }
