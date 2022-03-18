623ce3a1fb126dbfbf27f58c75f206b344180e0f
Adrian <mitura55@interia.pl>
2022-03-18 06:44:23 +0100

    Create OpinionOrderProductFilter.

---
+++ SupplementStore.Domain/Orders/OpinionOrderProductFilter.cs
@@ -0,0 +1,20 @@
+﻿using SupplementStore.Domain.Opinions;
+using System.Linq;
+
+namespace SupplementStore.Domain.Orders {
+
+    public class OpinionOrderProductFilter : IFilter<OrderProduct> {
+
+        public OpinionId OpinionId { get; }
+
+        public OpinionOrderProductFilter(OpinionId opinionId) {
+
+            OpinionId = opinionId;
+        }
+
+        public OrderProduct Process(IQueryable<OrderProduct> entities) {
+
+            return entities.FirstOrDefault(e => e.OpinionId == OpinionId);
+        }
+    }
+}
