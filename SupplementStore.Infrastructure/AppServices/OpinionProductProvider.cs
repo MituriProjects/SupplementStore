630cb985341dfa1b3822b1d39721ac15cd274c08
Adrian <mitura55@interia.pl>
2022-03-18 06:46:04 +0100

    Create OpinionProductProvider.

---
+++ SupplementStore.Infrastructure/AppServices/OpinionProductProvider.cs
@@ -0,0 +1,36 @@
+﻿using SupplementStore.Application.Models;
+using SupplementStore.Application.Services;
+using SupplementStore.Domain.Opinions;
+using SupplementStore.Domain.Orders;
+using SupplementStore.Domain.Products;
+
+namespace SupplementStore.Infrastructure.AppServices {
+
+    public class OpinionProductProvider : IOpinionProductProvider {
+
+        IOrderProductRepository OrderProductRepository { get; }
+
+        IProductRepository ProductRepository { get; }
+
+        public OpinionProductProvider(
+            IOrderProductRepository orderProductRepository,
+            IProductRepository productRepository) {
+
+            OrderProductRepository = orderProductRepository;
+            ProductRepository = productRepository;
+        }
+
+        public ProductDetails Load(string opinionId) {
+
+            var orderProduct = OrderProductRepository.FindBy(new OpinionOrderProductFilter(new OpinionId(opinionId)));
+
+            var product = ProductRepository.FindBy(orderProduct.ProductId);
+
+            return new ProductDetails {
+                Id = product.ProductId.ToString(),
+                Name = product.Name,
+                Price = product.Price
+            };
+        }
+    }
+}
