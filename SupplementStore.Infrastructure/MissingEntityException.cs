2a7a2addc25fa3b81e70d367053fdc2ee6f1e4ed
Adrian <mitura55@interia.pl>
2022-03-18 06:51:21 +0100

    Create MissingEntityException.

---
+++ SupplementStore.Infrastructure/MissingEntityException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SupplementStore.Infrastructure {
+
+    [Serializable]
+    public class MissingEntityException : Exception {
+
+        public MissingEntityException() {
+        }
+
+        public MissingEntityException(string message) : base(message) {
+        }
+
+        public MissingEntityException(string message, Exception innerException) : base(message, innerException) {
+        }
+
+        protected MissingEntityException(SerializationInfo info, StreamingContext context) : base(info, context) {
+        }
+    }
+}
