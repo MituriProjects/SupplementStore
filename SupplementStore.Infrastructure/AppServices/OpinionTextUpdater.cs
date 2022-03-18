76c80468d7e026f7adeb9ac08933459ea4534353
Adrian <mitura55@interia.pl>
2022-03-18 06:52:40 +0100

    Create OpinionTextUpdater.

---
+++ SupplementStore.Infrastructure/AppServices/OpinionTextUpdater.cs
@@ -0,0 +1,32 @@
+﻿using SupplementStore.Application.Services;
+using SupplementStore.Domain.Opinions;
+
+namespace SupplementStore.Infrastructure.AppServices {
+
+    public class OpinionTextUpdater : IOpinionTextUpdater {
+
+        IOpinionRepository OpinionRepository { get; }
+
+        IDocumentApprover DocumentApprover { get; }
+
+        public OpinionTextUpdater(
+            IOpinionRepository opinionRepository,
+            IDocumentApprover documentApprover) {
+
+            OpinionRepository = opinionRepository;
+            DocumentApprover = documentApprover;
+        }
+
+        public void Update(string opinionId, string text) {
+
+            var opinion = OpinionRepository.FindBy(new OpinionId(opinionId));
+
+            if (opinion == null)
+                throw new MissingEntityException("There is no opinion entity with the provided id.");
+
+            opinion.Text = text;
+
+            DocumentApprover.SaveChanges();
+        }
+    }
+}
