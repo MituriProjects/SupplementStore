4405c2229764ecf9b795932b4b55f33d52f83f18
Adrian <mitura55@interia.pl>
2022-03-18 10:32:58 +0100

    Create OpinionRevealer.

---
+++ SupplementStore.Infrastructure/AppServices/OpinionRevealer.cs
@@ -0,0 +1,32 @@
+﻿using SupplementStore.Application.Services;
+using SupplementStore.Domain.Opinions;
+
+namespace SupplementStore.Infrastructure.AppServices {
+
+    public class OpinionRevealer : IOpinionRevealer {
+
+        IOpinionRepository OpinionRepository { get; }
+
+        IDocumentApprover DocumentApprover { get; }
+
+        public OpinionRevealer(
+            IOpinionRepository opinionRepository,
+            IDocumentApprover documentApprover) {
+
+            OpinionRepository = opinionRepository;
+            DocumentApprover = documentApprover;
+        }
+
+        public void Reveal(string opinionId) {
+
+            var opinion = OpinionRepository.FindBy(new OpinionId(opinionId));
+
+            if (opinion == null)
+                throw new MissingEntityException("There is no opinion entity with the provided id.");
+
+            opinion.IsHidden = false;
+
+            DocumentApprover.SaveChanges();
+        }
+    }
+}
