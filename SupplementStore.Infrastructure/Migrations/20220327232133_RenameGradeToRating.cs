0c8f949ea942429f3cc3994f143745ae006f5c49
Adrian <mitura55@interia.pl>
2022-03-28 01:23:29 +0200

    Create a RenameGradeToRating migration.

---
+++ SupplementStore.Infrastructure/Migrations/20220327232133_RenameGradeToRating.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SupplementStore.Infrastructure.Migrations
+{
+    public partial class RenameGradeToRating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Grade_Stars",
+                table: "Opinions",
+                newName: "Rating_Stars");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Rating_Stars",
+                table: "Opinions",
+                newName: "Grade_Stars");
+        }
+    }
+}
