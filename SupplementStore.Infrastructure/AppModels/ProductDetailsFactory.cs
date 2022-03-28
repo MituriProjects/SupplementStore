12edc1cba7a26d4146008b0143b0eb45501dc32d
Adrian <mitura55@interia.pl>
2022-03-28 02:50:34 +0200

    Create ProductDetailsFactory.

---
+++ SupplementStore.Infrastructure/AppModels/ProductDetailsFactory.cs
@@ -0,0 +1,17 @@
+﻿using SupplementStore.Application.Models;
+using SupplementStore.Domain.Products;
+
+namespace SupplementStore.Infrastructure.AppModels {
+
+    static class ProductDetailsFactory {
+
+        public static ProductDetails Create(Product product) {
+
+            return new ProductDetails {
+                Id = product.ProductId.ToString(),
+                Name = product.Name,
+                Price = product.Price
+            };
+        }
+    }
+}
