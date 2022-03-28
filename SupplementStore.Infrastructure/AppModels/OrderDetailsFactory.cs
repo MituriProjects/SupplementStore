25743796ed8ed6f549470142c2a33d03b64204da
Adrian <mitura55@interia.pl>
2022-03-28 02:30:41 +0200

    Create OrderDetailsFactory.

---
+++ SupplementStore.Infrastructure/AppModels/OrderDetailsFactory.cs
@@ -0,0 +1,22 @@
+﻿using SupplementStore.Application.Models;
+using SupplementStore.Domain.Orders;
+using System.Collections.Generic;
+
+namespace SupplementStore.Infrastructure.AppModels {
+
+    static class OrderDetailsFactory {
+
+        public static OrderDetails Create(Order order, IEnumerable<PurchaseDetails> purchaseDetailsCollection) {
+
+            return new OrderDetails {
+                Id = order.OrderId.ToString(),
+                UserId = order.UserId,
+                Address = order.Address.Street,
+                PostalCode = order.Address.PostalCode,
+                City = order.Address.City,
+                CreatedOn = order.CreatedOn,
+                Purchases = purchaseDetailsCollection
+            };
+        }
+    }
+}
