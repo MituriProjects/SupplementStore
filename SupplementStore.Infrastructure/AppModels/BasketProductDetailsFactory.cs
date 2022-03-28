21945cdbf99965d9e0ada09e4138e332d3059300
Adrian <mitura55@interia.pl>
2022-03-28 02:58:16 +0200

    Create BasketProductDetailsFactory.

---
+++ SupplementStore.Infrastructure/AppModels/BasketProductDetailsFactory.cs
@@ -0,0 +1,20 @@
+﻿using SupplementStore.Application.Models;
+using SupplementStore.Domain.Baskets;
+using SupplementStore.Domain.Products;
+
+namespace SupplementStore.Infrastructure.AppModels {
+
+    static class BasketProductDetailsFactory {
+
+        public static BasketProductDetails Create(BasketProduct basketProduct, Product product) {
+
+            return new BasketProductDetails {
+                Id = basketProduct.BasketProductId.ToString(),
+                ProductId = basketProduct.ProductId.ToString(),
+                ProductName = product.Name,
+                ProductPrice = product.Price,
+                Quantity = basketProduct.Quantity
+            };
+        }
+    }
+}
