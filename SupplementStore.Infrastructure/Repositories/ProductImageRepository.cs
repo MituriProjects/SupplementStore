ac70788a6330298c829fda762e02c35ad0b7ba72
Adrian <mitura55@interia.pl>
2022-04-01 13:29:42 +0200

    Create ProductImageRepository.

---
+++ SupplementStore.Infrastructure/Repositories/ProductImageRepository.cs
@@ -0,0 +1,10 @@
+﻿using SupplementStore.Domain.Products;
+
+namespace SupplementStore.Infrastructure.Repositories {
+
+    public class ProductImageRepository : Repository<ProductImage>, IProductImageRepository {
+
+        public ProductImageRepository(IDocument<ProductImage> document) : base(document) {
+        }
+    }
+}
