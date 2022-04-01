5cd85999255387b259133c0bd39763bb7652c96b
Adrian <mitura55@interia.pl>
2022-04-01 13:50:14 +0200

    Create IFileWriter.

---
+++ SupplementStore/Controllers/Services/IFileWriter.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace SupplementStore.Controllers.Services {
+
+    public interface IFileWriter {
+        Task ProcessAsync(IFormFile formFile, params string[] pathPieces);
+    }
+}
