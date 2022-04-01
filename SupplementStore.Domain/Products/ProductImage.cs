68436374b85d677bef105e42a3be86a0892d910b
Adrian <mitura55@interia.pl>
2022-04-01 13:27:21 +0200

    Create ProductImage.

---
+++ SupplementStore.Domain/Products/ProductImage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SupplementStore.Domain.Products {
+
+    public class ProductImage : Entity {
+
+        public ProductImageId ProductImageId { get; private set; } = new ProductImageId(Guid.Empty);
+
+        Guid Product_Id {
+            get => ProductId.Id;
+            set => ProductId = new ProductId(value);
+        }
+
+        public ProductId ProductId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
