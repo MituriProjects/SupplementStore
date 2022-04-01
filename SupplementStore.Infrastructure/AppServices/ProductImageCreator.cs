93513fe75bdc68d48b8f342d72d8bc881a080691
Adrian <mitura55@interia.pl>
2022-04-01 13:32:12 +0200

    Create ProductImageCreator.

---
+++ SupplementStore.Infrastructure/AppServices/ProductImageCreator.cs
@@ -0,0 +1,42 @@
+﻿using SupplementStore.Application.Results;
+using SupplementStore.Application.Services;
+using SupplementStore.Domain.Products;
+
+namespace SupplementStore.Infrastructure.AppServices {
+
+    public class ProductImageCreator : IProductImageCreator {
+
+        IProductRepository ProductRepository { get; }
+
+        IProductImageRepository ProductImageRepository { get; }
+
+        IDomainApprover DomainApprover { get; }
+
+        public ProductImageCreator(
+            IProductRepository productRepository,
+            IProductImageRepository productImageRepository,
+            IDomainApprover domainApprover) {
+
+            ProductRepository = productRepository;
+            ProductImageRepository = productImageRepository;
+            DomainApprover = domainApprover;
+        }
+
+        public ProductImageCreatorResult Create(string productId, string imageName) {
+
+            var product = ProductRepository.FindBy(new ProductId(productId));
+
+            if (product == null)
+                return ProductImageCreatorResult.Failed;
+
+            ProductImageRepository.Add(new ProductImage {
+                ProductId = new ProductId(productId),
+                Name = imageName
+            });
+
+            DomainApprover.SaveChanges();
+
+            return ProductImageCreatorResult.Succeeded;
+        }
+    }
+}
