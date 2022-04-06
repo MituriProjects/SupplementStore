c030af003d91666d8ef809a32507b6b701bb181d
Adrian <mitura55@interia.pl>
2022-04-06 12:07:47 +0200

    Create ProductImagesFilter.

---
+++ SupplementStore.Domain/Products/ProductImagesFilter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SupplementStore.Domain.Products {
+
+    public class ProductImagesFilter : IManyFilter<ProductImage> {
+
+        ProductId ProductId { get; }
+
+        public ProductImagesFilter(ProductId productId) {
+
+            ProductId = productId;
+        }
+
+        public IEnumerable<ProductImage> Process(IQueryable<ProductImage> entities) {
+
+            return entities.Where(e => e.ProductId == ProductId);
+        }
+    }
+}
