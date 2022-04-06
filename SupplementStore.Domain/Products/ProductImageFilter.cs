1d2a3eb2bbf643ffed90c65bbd54b814cd5959e8
Adrian <mitura55@interia.pl>
2022-04-06 14:22:51 +0200

    Create ProductImageFilter.

---
+++ SupplementStore.Domain/Products/ProductImageFilter.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace SupplementStore.Domain.Products {
+
+    public class ProductImageFilter : IFilter<ProductImage> {
+
+        ProductId ProductId { get; }
+
+        string Name { get; }
+
+        public ProductImageFilter(ProductId productId, string name) {
+
+            ProductId = productId;
+            Name = name;
+        }
+
+        public ProductImage Process(IQueryable<ProductImage> entities) {
+
+            return entities.FirstOrDefault(e => e.ProductId == ProductId && e.Name == Name);
+        }
+    }
+}
