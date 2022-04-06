0c1a0ccb736c1304c9158cb205060c270fef254b
Adrian <mitura55@interia.pl>
2022-04-06 19:41:43 +0200

    Overload a FindBy method in ProductImageRepository.

--- SupplementStore.Infrastructure/Repositories/ProductImageRepository.cs
+++ SupplementStore.Infrastructure/Repositories/ProductImageRepository.cs
@@ -1,10 +1,17 @@
 ﻿using SupplementStore.Domain.Products;
+using System.Linq;
 
 namespace SupplementStore.Infrastructure.Repositories {
 
     public class ProductImageRepository : Repository<ProductImage>, IProductImageRepository {
 
         public ProductImageRepository(IDocument<ProductImage> document) : base(document) {
+        }
+
+        public ProductImage FindBy(ProductImageId productImageId) {
+
+            return Document.All
+                .FirstOrDefault(e => e.ProductImageId == productImageId);
         }
     }
 }
