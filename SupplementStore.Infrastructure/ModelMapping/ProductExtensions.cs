cbdfbb9239935935cd13b2876376cf92cc7991ba
Adrian <mitura55@interia.pl>
2022-04-09 01:11:27 +0200

    Create a Product's ToDetails extension method.

---
+++ SupplementStore.Infrastructure/ModelMapping/ProductExtensions.cs
@@ -0,0 +1,18 @@
+﻿using SupplementStore.Application.Models;
+using SupplementStore.Domain.Products;
+
+namespace SupplementStore.Infrastructure.ModelMapping {
+
+    static class ProductExtensions {
+
+        public static ProductDetails ToDetails(this Product product, ProductImage productImage = null) {
+
+            return new ProductDetails {
+                Id = product.ProductId.ToString(),
+                Name = product.Name,
+                Price = product.Price,
+                MainImage = productImage?.Name
+            };
+        }
+    }
+}
