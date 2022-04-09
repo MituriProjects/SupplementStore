c5a9c0e29909973e95dcc9f03155cd2bb79097a3
Adrian <mitura55@interia.pl>
2022-04-09 13:46:43 +0200

    Create OrderService.

---
+++ SupplementStore.Infrastructure/AppServices/Order/Create.cs
@@ -0,0 +1,20 @@
+﻿using SupplementStore.Application.Args;
+using SupplementStore.Application.Models;
+using SupplementStore.Infrastructure.ArgsMapping;
+
+namespace SupplementStore.Infrastructure.AppServices.Order {
+
+    public partial class OrderService {
+
+        public OrderDetails Create(OrderCreatorArgs args) {
+
+            var order = OrderFactory.Create(args.ToOrderFactoryArgs());
+
+            DomainApprover.SaveChanges();
+
+            return new OrderDetails {
+                Id = order.OrderId.ToString()
+            };
+        }
+    }
+}
---
+++ SupplementStore.Infrastructure/AppServices/Order/Dependencies.cs
@@ -0,0 +1,33 @@
+﻿using SupplementStore.Application.Services;
+using SupplementStore.Domain.Orders;
+using SupplementStore.Domain.Products;
+
+namespace SupplementStore.Infrastructure.AppServices.Order {
+
+    public partial class OrderService : IOrderService {
+
+        IOrderRepository OrderRepository { get; }
+
+        IPurchaseRepository PurchaseRepository { get; }
+
+        IProductRepository ProductRepository { get; }
+
+        OrderFactory OrderFactory { get; }
+
+        IDomainApprover DomainApprover { get; }
+
+        public OrderService(
+            IOrderRepository orderRepository,
+            IPurchaseRepository purchaseRepository,
+            IProductRepository productRepository,
+            OrderFactory orderFactory,
+            IDomainApprover domainApprover) {
+
+            OrderRepository = orderRepository;
+            PurchaseRepository = purchaseRepository;
+            ProductRepository = productRepository;
+            OrderFactory = orderFactory;
+            DomainApprover = domainApprover;
+        }
+    }
+}
