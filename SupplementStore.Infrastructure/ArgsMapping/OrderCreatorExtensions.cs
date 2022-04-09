35ad483af4e04c6fcde126940a8e2f91c0e0b374
Adrian <mitura55@interia.pl>
2022-04-09 13:37:26 +0200

    Create an OrderCreatorArgs' ToOrderFactoryArgs extension method.

---
+++ SupplementStore.Infrastructure/ArgsMapping/OrderCreatorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using SupplementStore.Application.Args;
+using SupplementStore.Domain.Orders;
+
+namespace SupplementStore.Infrastructure.ArgsMapping {
+
+    static class OrderCreatorExtensions {
+
+        public static OrderFactoryArgs ToOrderFactoryArgs(this OrderCreatorArgs orderCreatorArgs) {
+
+            return new OrderFactoryArgs {
+                UserId = orderCreatorArgs.UserId,
+                Address = orderCreatorArgs.Address,
+                PostalCode = orderCreatorArgs.PostalCode,
+                City = orderCreatorArgs.City
+            };
+        }
+    }
+}
