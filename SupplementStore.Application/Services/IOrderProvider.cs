6a60be0d63acdd0f4f5970812833f2f43ee24c43
Adrian <mitura55@interia.pl>
2022-04-09 13:52:25 +0200

    Delete IOrderProvider.

--- SupplementStore.Application/Services/IOrderProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿using SupplementStore.Application.Models;
-
-namespace SupplementStore.Application.Services {
-
-    public interface IOrderProvider {
-        OrderDetails Load(string id);
-    }
-}
