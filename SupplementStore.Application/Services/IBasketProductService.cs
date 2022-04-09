58223a1501af743253f6a06989d417bd53be3c8c
Adrian <mitura55@interia.pl>
2022-04-09 02:36:47 +0200

    Create IBasketProductService.

---
+++ SupplementStore.Application/Services/IBasketProductService.cs
@@ -0,0 +1,13 @@
+﻿using SupplementStore.Application.Models;
+using System.Collections.Generic;
+
+namespace SupplementStore.Application.Services {
+
+    public interface IBasketProductService {
+        BasketProductDetails Load(string Id);
+        IEnumerable<BasketProductDetails> LoadMany(string userId);
+        void Create(string userId, string productId, int quantity);
+        void Update(BasketProductDetails basketProductDetails);
+        void Remove(string userId, string productId);
+    }
+}
