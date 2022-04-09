9571adf73f367bb4f4f93ace9e40b7e8bf54bb0f
Adrian <mitura55@interia.pl>
2022-04-09 13:15:06 +0200

    Delete IBasketProductUpdater.

--- SupplementStore.Application/Services/IBasketProductUpdater.cs
+++
@@ -1,8 +0,0 @@
-﻿using SupplementStore.Application.Models;
-
-namespace SupplementStore.Application.Services {
-
-    public interface IBasketProductUpdater {
-        void Update(BasketProductDetails basketProductDetails);
-    }
-}
