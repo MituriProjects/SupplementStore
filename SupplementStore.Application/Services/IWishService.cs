9d0aacab9c4cefe8441197c1f36458888c2fba7e
Adrian <mitura55@interia.pl>
2022-04-11 16:40:14 +0200

    Create IWishService.

---
+++ SupplementStore.Application/Services/IWishService.cs
@@ -0,0 +1,12 @@
+﻿using SupplementStore.Application.Models;
+using System.Collections.Generic;
+
+namespace SupplementStore.Application.Services {
+
+    public interface IWishService {
+        IEnumerable<ProductDetails> LoadMany(string userId);
+        bool IsOnWishList(string userId, string productId);
+        void Create(string userId, string productId);
+        void Remove(string userId, string productId);
+    }
+}
