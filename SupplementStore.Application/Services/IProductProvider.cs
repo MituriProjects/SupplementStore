3104475e2da320b96e85211b3eceac6a0b2c69f9
Adrian <mitura55@interia.pl>
2022-04-11 13:51:12 +0200

    Delete IProductProvider.

--- SupplementStore.Application/Services/IProductProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿using SupplementStore.Application.Models;
-
-namespace SupplementStore.Application.Services {
-
-    public interface IProductProvider {
-        ProductDetails Load(string productId);
-    }
-}
