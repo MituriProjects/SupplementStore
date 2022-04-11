01303207cbbaa8dd78279c3af3ae184b5f52499b
Adrian <mitura55@interia.pl>
2022-04-11 13:39:11 +0200

    Create IProductService.

---
+++ SupplementStore.Application/Services/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using SupplementStore.Application.Args;
+using SupplementStore.Application.Models;
+using SupplementStore.Application.Results;
+using System.Collections.Generic;
+
+namespace SupplementStore.Application.Services {
+
+    public interface IProductService {
+        ProductDetails Load(string productId);
+        ProductsProviderResult LoadMany(ProductsProviderArgs args);
+        IEnumerable<ProductOpinionDetails> LoadOpinions(string productId);
+        ProductDetails Create(string name, decimal price);
+        void Update(ProductDetails productDetails);
+    }
+}
