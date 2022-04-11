23254482940b123279ad69312f8b8ab8ea86d226
Adrian <mitura55@interia.pl>
2022-04-11 17:39:43 +0200

    Delete IWishesProvider.

--- SupplementStore.Application/Services/IWishesProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using SupplementStore.Application.Models;
-using System.Collections.Generic;
-
-namespace SupplementStore.Application.Services {
-
-    public interface IWishesProvider {
-        IEnumerable<ProductDetails> Load(string userId);
-    }
-}
