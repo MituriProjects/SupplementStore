aeaebfc5b452f0b7b0e0ebe4eb2444ffe9f63508
Adrian <mitura55@interia.pl>
2022-04-11 18:51:23 +0200

    Delete IUserOpinionsProvider.

--- SupplementStore.Application/Services/IUserOpinionsProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using SupplementStore.Application.Models;
-using System.Collections.Generic;
-
-namespace SupplementStore.Application.Services {
-
-    public interface IUserOpinionsProvider {
-        IEnumerable<OpinionDetails> Load(string userId);
-    }
-}
