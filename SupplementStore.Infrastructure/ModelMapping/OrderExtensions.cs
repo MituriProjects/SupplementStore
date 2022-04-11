65a3b1bef114758734ec072b547b28a18bbd80e9
Adrian <mitura55@interia.pl>
2022-04-11 19:30:53 +0200

    Create an Order's ToDetails extension method.

---
+++ SupplementStore.Infrastructure/ModelMapping/OrderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using SupplementStore.Application.Models;
+using SupplementStore.Domain.Orders;
+using System.Collections.Generic;
+
+namespace SupplementStore.Infrastructure.ModelMapping {
+
+    static class OrderExtensions {
+
+        public static OrderDetails ToDetails(this Order order, IEnumerable<PurchaseDetails> purchaseDetailsCollection) {
+
+            return new OrderDetails {
+                Id = order.OrderId.ToString(),
+                UserId = order.UserId,
+                Address = order.Address.Street,
+                PostalCode = order.Address.PostalCode,
+                City = order.Address.City,
+                CreatedOn = order.CreatedOn,
+                Purchases = purchaseDetailsCollection
+            };
+        }
+    }
+}
