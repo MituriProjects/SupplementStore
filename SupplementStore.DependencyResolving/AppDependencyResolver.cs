d64206021ae539afda49cf192941f7af1cbbdcdb
Adrian <mitura55@interia.pl>
2022-04-11 18:52:23 +0200

    Remove the IHiddenOpinionsProvider dependency from AppDependencyResolver.

--- SupplementStore.DependencyResolving/AppDependencyResolver.cs
+++ SupplementStore.DependencyResolving/AppDependencyResolver.cs
@@ -43,7 +43,6 @@ namespace SupplementStore.DependencyResolving {
             services.AddTransient<IOrderService, OrderService>();
             services.AddTransient<IWishService, WishService>();
             services.AddTransient<IOpinionService, OpinionService>();
-            services.AddTransient<IHiddenOpinionsProvider, HiddenOpinionsProvider>();
             services.AddTransient<IOpinionCreator, OpinionCreator>();
             services.AddTransient<IOpinionTextUpdater, OpinionTextUpdater>();
             services.AddTransient<IOpinionHider, OpinionHider>();
