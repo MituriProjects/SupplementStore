2d575241657dd93cda8cb0bbe881a8faae1ddb93
Adrian <mitura55@interia.pl>
2022-04-11 14:24:44 +0200

    Create IProductImageService.

---
+++ SupplementStore.Application/Services/IProductImageService.cs
@@ -0,0 +1,13 @@
+﻿using SupplementStore.Application.Models;
+using SupplementStore.Application.Results;
+using System.Collections.Generic;
+
+namespace SupplementStore.Application.Services {
+
+    public interface IProductImageService {
+        IEnumerable<ProductImageDetails> LoadMany(string productId);
+        ProductImageCreatorResult Create(string productId, string imageName);
+        ProductImageRemoverResult Remove(string productId, string name);
+        void AppointMain(string productId, string name);
+    }
+}
