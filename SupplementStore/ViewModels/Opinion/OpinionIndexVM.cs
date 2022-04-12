a908d26570745824cf100d60adfb76bcce08a146
Adrian <mitura55@interia.pl>
2022-04-13 00:49:49 +0200

    Rename Opinion's OpinionIndexViewModel to OpinionIndexVM.

--- SupplementStore/ViewModels/Opinion/OpinionIndexVM.cs
+++ SupplementStore/ViewModels/Opinion/OpinionIndexVM.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace SupplementStore.ViewModels.Opinion {
 
-    public class OpinionIndexViewModel {
+    public class OpinionIndexVM {
 
         public bool IsProductToOpineWaiting { get; set; }
 
