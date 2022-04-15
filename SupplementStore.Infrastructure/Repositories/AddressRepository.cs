56a4d0c2dff09aa9658cf0ca4a2aab52959c79b9
Adrian <mitura55@interia.pl>
2022-04-15 07:37:13 +0200

    Create AddressRepository.

---
+++ SupplementStore.Infrastructure/Repositories/AddressRepository.cs
@@ -0,0 +1,10 @@
+﻿using SupplementStore.Domain.Addresses;
+
+namespace SupplementStore.Infrastructure.Repositories {
+
+    public class AddressRepository : Repository<Address>, IAddressRepository {
+
+        public AddressRepository(IDocument<Address> document) : base(document) {
+        }
+    }
+}
