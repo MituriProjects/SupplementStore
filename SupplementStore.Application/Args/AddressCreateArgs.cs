483e1b03aafe78f0abceb3429c8a8aad52a79d55
Adrian <mitura55@interia.pl>
2022-04-15 11:25:05 +0200

    Create AddressCreateArgs.

---
+++ SupplementStore.Application/Args/AddressCreateArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace SupplementStore.Application.Args {
+
+    public class AddressCreateArgs {
+
+        public string UserId { get; set; }
+
+        public string Street { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string City { get; set; }
+    }
+}
