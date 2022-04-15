299f6a972f7ff1d78877bdc921ad9b75ea3c3e90
Adrian <mitura55@interia.pl>
2022-04-15 11:26:52 +0200

    Create AddressService.

---
+++ SupplementStore.Infrastructure/AppServices/Addresses/Create.cs
@@ -0,0 +1,16 @@
+﻿using SupplementStore.Application.Args;
+using SupplementStore.Domain.Addresses;
+using SupplementStore.Infrastructure.ArgsMapping;
+
+namespace SupplementStore.Infrastructure.AppServices.Addresses {
+
+    public partial class AddressService {
+
+        public void Create(AddressCreateArgs args) {
+
+            AddressFactory.Create(args.ToFactoryArgs());
+
+            DomainApprover.SaveChanges();
+        }
+    }
+}
---
+++ SupplementStore.Infrastructure/AppServices/Addresses/Dependencies.cs
@@ -0,0 +1,20 @@
+﻿using SupplementStore.Application.Services;
+using SupplementStore.Domain.Addresses;
+
+namespace SupplementStore.Infrastructure.AppServices.Addresses {
+
+    public partial class AddressService : IAddressService {
+
+        AddressFactory AddressFactory { get; }
+
+        IDomainApprover DomainApprover { get; }
+
+        public AddressService(
+            AddressFactory addressFactory,
+            IDomainApprover domainApprover) {
+
+            AddressFactory = addressFactory;
+            DomainApprover = domainApprover;
+        }
+    }
+}
