e50f96f03fd2d037745c08355c5c33f71f7055b5
Adrian <mitura55@interia.pl>
2022-04-15 07:30:30 +0200

    Create Address.

---
+++ SupplementStore.Domain/Addresses/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SupplementStore.Domain.Addresses {
+
+    public class Address : Entity {
+
+        public AddressId AddressId { get; private set; } = new AddressId(Guid.Empty);
+
+        public string UserId { get; set; }
+
+        public string Street { get; set; }
+
+        public PostalCode PostalCode { get; set; }
+
+        public string City { get; set; }
+
+        public bool IsHidden { get; set; }
+    }
+}
