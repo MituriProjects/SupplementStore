ca3bd9ba04ce4b1048e74766ee9782186978c480
Adrian <mitura55@interia.pl>
2022-04-15 11:27:59 +0200

    Create an Address's Create view model.

---
+++ SupplementStore/ViewModels/Address/CreateVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupplementStore.ViewModels.Address {
+
+    public class CreateVM {
+
+        [Required(ErrorMessage = "Nazwa ulicy jest wymagana.")]
+        [Display(Name = "Ulica")]
+        public string Street { get; set; }
+
+        [Required(ErrorMessage = "Kod pocztowy jest wymagany.")]
+        [RegularExpression(@"^\d{2}-\d{3}$", ErrorMessage = "Kod pocztowy ma niewłaściwy format.")]
+        [Display(Name = "Kod pocztowy")]
+        public string PostalCode { get; set; }
+
+        [Required(ErrorMessage = "Nazwa miejscowości jest wymagana.")]
+        [Display(Name = "Miasto")]
+        public string City { get; set; }
+    }
+}
