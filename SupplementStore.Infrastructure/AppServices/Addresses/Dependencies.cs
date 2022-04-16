ce2d6429a1bf9e7be5c2670e1d62c947b288d0df
Adrian <mitura55@interia.pl>
2022-04-16 20:54:26 +0200

    Add a LoadMany method to AddressService.

--- SupplementStore.Infrastructure/AppServices/Addresses/Dependencies.cs
+++ SupplementStore.Infrastructure/AppServices/Addresses/Dependencies.cs
@@ -5,14 +5,18 @@ namespace SupplementStore.Infrastructure.AppServices.Addresses {
 
     public partial class AddressService : IAddressService {
 
+        IAddressRepository AddressRepository { get; }
+
         AddressFactory AddressFactory { get; }
 
         IDomainApprover DomainApprover { get; }
 
         public AddressService(
+            IAddressRepository addressRepository,
             AddressFactory addressFactory,
             IDomainApprover domainApprover) {
 
+            AddressRepository = addressRepository;
             AddressFactory = addressFactory;
             DomainApprover = domainApprover;
         }
---
+++ SupplementStore.Infrastructure/AppServices/Addresses/LoadMany.cs
@@ -0,0 +1,17 @@
+﻿using SupplementStore.Application.Models;
+using SupplementStore.Domain.Addresses;
+using SupplementStore.Infrastructure.ModelMapping;
+using System.Collections.Generic;
+
+namespace SupplementStore.Infrastructure.AppServices.Addresses {
+
+    public partial class AddressService {
+
+        public IEnumerable<AddressDetails> LoadMany(string userId) {
+
+            return AddressRepository
+                .FindBy(new UserAddressesFilter(userId))
+                .ToDetails();
+        }
+    }
+}
