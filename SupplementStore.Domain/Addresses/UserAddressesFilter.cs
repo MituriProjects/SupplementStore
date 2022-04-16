68bc13802952d9c6867c5ad7f3a202d831e03da9
Adrian <mitura55@interia.pl>
2022-04-16 20:53:58 +0200

    Create UserAddressesFilter.

---
+++ SupplementStore.Domain/Addresses/UserAddressesFilter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SupplementStore.Domain.Addresses {
+
+    public class UserAddressesFilter : IManyFilter<Address> {
+
+        string UserId { get; }
+
+        public UserAddressesFilter(string userId) {
+
+            UserId = userId;
+        }
+
+        public IEnumerable<Address> Process(IQueryable<Address> entities) {
+
+            return entities.Where(e => e.UserId == UserId && e.IsHidden == false);
+        }
+    }
+}
