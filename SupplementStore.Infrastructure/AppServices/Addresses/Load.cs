468b0ef680e92b27211e095b16b6bd661409bf96
Adrian <mitura55@interia.pl>
2022-04-16 21:51:47 +0200

    Add a Load method to AddressService.

---
+++ SupplementStore.Infrastructure/AppServices/Addresses/Load.cs
@@ -0,0 +1,16 @@
+﻿using SupplementStore.Application.Models;
+using SupplementStore.Domain.Addresses;
+using SupplementStore.Infrastructure.ModelMapping;
+
+namespace SupplementStore.Infrastructure.AppServices.Addresses {
+
+    public partial class AddressService {
+
+        public AddressDetails Load(string addressId) {
+
+            return AddressRepository
+                .FindBy(new AddressId(addressId))?
+                .ToDetails();
+        }
+    }
+}
