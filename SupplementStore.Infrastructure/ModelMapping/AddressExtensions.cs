3026a980b6864fbe6167f5bcf1d2bbf1964f4b8e
Adrian <mitura55@interia.pl>
2022-04-16 20:53:48 +0200

    Create an Address's ToDetails extension method.

---
+++ SupplementStore.Infrastructure/ModelMapping/AddressExtensions.cs
@@ -0,0 +1,20 @@
+﻿using SupplementStore.Application.Models;
+using SupplementStore.Domain.Addresses;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SupplementStore.Infrastructure.ModelMapping {
+
+    static class AddressExtensions {
+
+        public static IEnumerable<AddressDetails> ToDetails(this IEnumerable<Address> addresses) {
+
+            return addresses.Select(e => new AddressDetails {
+                Id = e.AddressId.ToString(),
+                Street = e.Street,
+                PostalCode = e.PostalCode.Value,
+                City = e.City
+            }).ToList();
+        }
+    }
+}
