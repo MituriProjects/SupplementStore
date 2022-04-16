c20a55ad44cf2233a0427e06342588b1f2092647
Adrian <mitura55@interia.pl>
2022-04-16 20:53:40 +0200

    Create AddressDetails.

---
+++ SupplementStore.Application/Models/AddressDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace SupplementStore.Application.Models {
+
+    public class AddressDetails {
+
+        public string Id { get; set; }
+
+        public string Street { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string City { get; set; }
+    }
+}
