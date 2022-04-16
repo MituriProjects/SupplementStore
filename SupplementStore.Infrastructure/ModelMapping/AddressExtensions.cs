657cdca455beb0d51338b95154ee4f27ae4cad0b
Adrian <mitura55@interia.pl>
2022-04-16 21:47:25 +0200

    Overload the Address's ToDetails extension method.

--- SupplementStore.Infrastructure/ModelMapping/AddressExtensions.cs
+++ SupplementStore.Infrastructure/ModelMapping/AddressExtensions.cs
@@ -7,14 +7,21 @@ namespace SupplementStore.Infrastructure.ModelMapping {
 
     static class AddressExtensions {
 
+        public static AddressDetails ToDetails(this Address address) {
+
+            return new AddressDetails {
+                Id = address.AddressId.ToString(),
+                Street = address.Street,
+                PostalCode = address.PostalCode.Value,
+                City = address.City
+            };
+        }
+
         public static IEnumerable<AddressDetails> ToDetails(this IEnumerable<Address> addresses) {
 
-            return addresses.Select(e => new AddressDetails {
-                Id = e.AddressId.ToString(),
-                Street = e.Street,
-                PostalCode = e.PostalCode.Value,
-                City = e.City
-            }).ToList();
+            return addresses
+                .Select(e => e.ToDetails())
+                .ToList();
         }
     }
 }
