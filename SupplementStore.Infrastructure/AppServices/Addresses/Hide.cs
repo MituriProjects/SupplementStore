777e9f96518e4c323585025c8fe06671d63f6f14
Adrian <mitura55@interia.pl>
2022-04-17 21:07:53 +0200

    Add a Hide method to AddressService.

---
+++ SupplementStore.Infrastructure/AppServices/Addresses/Hide.cs
@@ -0,0 +1,22 @@
+﻿using SupplementStore.Domain.Addresses;
+
+namespace SupplementStore.Infrastructure.AppServices.Addresses {
+
+    public partial class AddressService {
+
+        public void Hide(string userId, string addressId) {
+
+            var address = AddressRepository.FindBy(new AddressId(addressId));
+
+            if (address == null)
+                return;
+
+            if (address.UserId != userId)
+                return;
+
+            address.IsHidden = true;
+
+            DomainApprover.SaveChanges();
+        }
+    }
+}
