6487cc4d3eb5a74034871f904d7393c5a9d8aa14
Adrian <mitura55@interia.pl>
2022-04-17 09:29:50 +0200

    Create AddressUpdateArgs.

---
+++ SupplementStore.Application/Args/AddressUpdateArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace SupplementStore.Application.Args {
+
+    public class AddressUpdateArgs {
+
+        public string Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Street { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string City { get; set; }
+    }
+}
