0a2932bab1d8d94fe6bf5a114f820eb78004fd61
Adrian <mitura55@interia.pl>
2022-04-27 13:45:00 +0200

    Prepare Product's ProductEditVM for localization.

--- SupplementStore/ViewModels/Product/ProductEditVM.cs
+++ SupplementStore/ViewModels/Product/ProductEditVM.cs
@@ -6,13 +6,13 @@ namespace SupplementStore.ViewModels.Product {
 
         public string Id { get; set; }
 
-        [Required(ErrorMessage = "Nazwa jest wymagana")]
-        [Display(Name = "Nazwa")]
+        [Required(ErrorMessage = "NameRequiredErrorMessage")]
+        [Display(Name = "NameLabel")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Cena jest wymagana")]
-        [Display(Name = "Cena")]
-        [Range(0.01, double.MaxValue, ErrorMessage = "Cena musi być powyżej zera")]
+        [Required(ErrorMessage = "PriceRequiredErrorMessage")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "PriceAboveZeroErrorMessage")]
+        [Display(Name = "PriceLabel")]
         public decimal? Price { get; set; } = 0;
     }
 }
