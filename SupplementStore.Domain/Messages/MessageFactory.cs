2cf42348acd5d2af23560c909ce4e80184a82530
Adrian <mitura55@interia.pl>
2022-05-03 14:19:54 +0200

    Create MessageFactory.

---
+++ SupplementStore.Domain/Messages/MessageFactory.cs
@@ -0,0 +1,25 @@
+﻿namespace SupplementStore.Domain.Messages {
+
+    public class MessageFactory {
+
+        IMessageRepository MessageRepository { get; }
+
+        public MessageFactory(IMessageRepository messageRepository) {
+
+            MessageRepository = messageRepository;
+        }
+
+        public Message Create(MessageFactoryArgs args) {
+
+            var message = new Message {
+                Text = args.Text,
+                UserId = args.UserId,
+                SenderEmail = args.UserId == null ? args.SenderEmail : null
+            };
+
+            MessageRepository.Add(message);
+
+            return message;
+        }
+    }
+}
