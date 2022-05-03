bd2d9649b544003ecf41ac8efcfae278743ce386
Adrian <mitura55@interia.pl>
2022-05-03 14:19:23 +0200

    Create MessageFactoryArgs.

---
+++ SupplementStore.Domain/Messages/MessageFactoryArgs.cs
@@ -0,0 +1,11 @@
+﻿namespace SupplementStore.Domain.Messages {
+
+    public class MessageFactoryArgs {
+
+        public string Text { get; set; }
+
+        public string UserId { get; set; }
+
+        public string SenderEmail { get; set; }
+    }
+}
