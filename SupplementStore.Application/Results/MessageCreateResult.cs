e2f2fc03a2d76da2833ba5b1aece9e503befbdd4
Adrian <mitura55@interia.pl>
2022-05-03 14:44:19 +0200

    Create MessageCreateResult.

---
+++ SupplementStore.Application/Results/MessageCreateResult.cs
@@ -0,0 +1,11 @@
+﻿namespace SupplementStore.Application.Results {
+
+    public class MessageCreateResult {
+
+        public bool Success { get; set; }
+
+        public static MessageCreateResult Succeeded = new MessageCreateResult { Success = true };
+
+        public static MessageCreateResult Failed = new MessageCreateResult { Success = false };
+    }
+}
