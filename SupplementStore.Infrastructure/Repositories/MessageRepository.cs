23b257e9fa84d9f4aae2e8629b9d36c86670952b
Adrian <mitura55@interia.pl>
2022-05-03 14:17:31 +0200

    Create MessageRepository.

---
+++ SupplementStore.Infrastructure/Repositories/MessageRepository.cs
@@ -0,0 +1,10 @@
+﻿using SupplementStore.Domain.Messages;
+
+namespace SupplementStore.Infrastructure.Repositories {
+
+    public class MessageRepository : Repository<Message>, IMessageRepository {
+
+        public MessageRepository(IDocument<Message> document) : base(document) {
+        }
+    }
+}
