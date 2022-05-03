f136d004e9e0957f305b761b7fa1a0f87bcc53a9
Adrian <mitura55@interia.pl>
2022-05-03 14:14:52 +0200

    Create Message.

---
+++ SupplementStore.Domain/Messages/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SupplementStore.Domain.Messages {
+
+    public class Message : Entity {
+
+        public MessageId MessageId { get; private set; } = new MessageId(Guid.Empty);
+
+        public string SenderEmail { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Text { get; set; }
+    }
+}
