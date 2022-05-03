439a18b5f580d9169784f90fffc4d8007bc3cc10
Adrian <mitura55@interia.pl>
2022-05-03 14:46:01 +0200

    Create MessageService.

---
+++ SupplementStore.Infrastructure/AppServices/Messages/Create.cs
@@ -0,0 +1,22 @@
+﻿using SupplementStore.Application.Args;
+using SupplementStore.Application.Results;
+using SupplementStore.Domain.Messages;
+using SupplementStore.Infrastructure.ArgsMapping;
+
+namespace SupplementStore.Infrastructure.AppServices.Messages {
+
+    public partial class MessageService {
+
+        public MessageCreateResult Create(MessageCreateArgs args) {
+
+            var message = MessageFactory.Create(args.ToFactoryArgs());
+
+            if (message == null)
+                return MessageCreateResult.Failed;
+
+            DomainApprover.SaveChanges();
+
+            return MessageCreateResult.Succeeded;
+        }
+    }
+}
---
+++ SupplementStore.Infrastructure/AppServices/Messages/Dependencies.cs
@@ -0,0 +1,20 @@
+﻿using SupplementStore.Application.Services;
+using SupplementStore.Domain.Messages;
+
+namespace SupplementStore.Infrastructure.AppServices.Messages {
+
+    public partial class MessageService : IMessageService {
+
+        MessageFactory MessageFactory { get; }
+
+        IDomainApprover DomainApprover { get; }
+
+        public MessageService(
+            MessageFactory messageFactory,
+            IDomainApprover domainApprover) {
+
+            MessageFactory = messageFactory;
+            DomainApprover = domainApprover;
+        }
+    }
+}
