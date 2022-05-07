2049f051864e8870a7244e38744c9af7fa5a19f0
Adrian <mitura55@interia.pl>
2022-05-07 03:04:02 +0200

    Create an Email validation attribute.

---
+++ SupplementStore/ViewModels/EmailAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupplementStore.ViewModels {
+
+    public class EmailAttribute : ValidationAttribute {
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext) {
+
+            return new EmailAddressAttribute().IsValid(value)
+                ? ValidationResult.Success
+                : new ValidationResult(GetErrorMessage(validationContext));
+        }
+
+        private string GetErrorMessage(ValidationContext validationContext) {
+
+            Translator translation = validationContext.GetService(typeof(Translator)) as Translator;
+
+            var errorMessageKey = "EmailErrorMessage";
+            var errorMessage = translation.GetLocalizedText(errorMessageKey);
+
+            return errorMessage == errorMessageKey
+                ? "Invalid e-mail."
+                : errorMessage;
+        }
+    }
+}
