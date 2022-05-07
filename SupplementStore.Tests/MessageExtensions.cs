4c0afc8dccdde59b5e98c70a733c1eeb407265d3
Adrian <mitura55@interia.pl>
2022-05-07 02:53:47 +0200

    Create a Message's WithUserId extension method.

---
+++ SupplementStore.Tests/MessageExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SupplementStore.Domain.Messages;
+
+namespace SupplementStore.Tests {
+
+    static class MessageExtensions {
+
+        public static Message WithUserId(this Message message, IdentityUser user) {
+
+            message.UserId = user.Id;
+
+            return message;
+        }
+    }
+}
