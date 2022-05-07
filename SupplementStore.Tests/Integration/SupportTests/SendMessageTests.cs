ee541bcfa5c724c33e36e59e6241e2921446e4a3
Adrian <mitura55@interia.pl>
2022-05-07 03:24:23 +0200

    Create Support's SendMessage actions.

---
+++ SupplementStore.Tests/Integration/SupportTests/SendMessageTests.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SupplementStore.Domain.Messages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SupplementStore.Tests.Integration.SupportTests {
+
+    [TestClass]
+    public class SendMessageTests : IntegrationTestsBase {
+
+        [TestMethod]
+        public async Task Post_DataIsValid_RedirectsToSendMessage() {
+
+            var formData = new Dictionary<string, string> {
+                { "Text", "TestText" },
+                { "Email", "newuser@test.pl" }
+            };
+
+            await PostAsync("/Support/SendMessage", formData);
+
+            ExamineRedirect("/Support/SendMessage");
+        }
+
+        [TestMethod]
+        public async Task Post_UserIsLoggedOut_CreatesMessage() {
+
+            var formData = new Dictionary<string, string> {
+                { "Text", "TestText" },
+                { "Email", TestData.User.Email }
+            };
+
+            await PostAsync("/Support/SendMessage", formData);
+
+            TestDocument<Message>.Single(e => e.Text == formData["Text"] && e.SenderEmail == null && e.UserId == TestData.User.Id);
+            TestDocumentApprover.ExamineSaveChanges();
+        }
+
+        [TestMethod]
+        public async Task Post_NoUserWithThisEmail_CreatesMessage() {
+
+            var formData = new Dictionary<string, string> {
+                { "Text", "TestText" },
+                { "Email", "unknownuser@test.pl" }
+            };
+
+            await PostAsync("/Support/SendMessage", formData);
+
+            TestDocument<Message>.Single(e => e.Text == formData["Text"] && e.SenderEmail == formData["Email"] && e.UserId == null);
+            TestDocumentApprover.ExamineSaveChanges();
+        }
+
+        [TestMethod]
+        public async Task Post_UserIsLoggedIn_CreatesMessage() {
+
+            var formData = new Dictionary<string, string> {
+                { "Text", "TestText" },
+                { "Email", TestData.User.Email }
+            };
+
+            await PostAsync("/Support/SendMessage", formData, TestData.User);
+
+            TestDocument<Message>.Single(e => e.Text == formData["Text"] && e.SenderEmail == null && e.UserId == TestData.User.Id);
+            TestDocumentApprover.ExamineSaveChanges();
+        }
+
+        [TestMethod]
+        public async Task Post_TextIsEmpty_NoMessageCreated() {
+
+            var formData = new Dictionary<string, string> {
+                { "Text", "" },
+                { "Email", "unknownuser@test.pl" }
+            };
+
+            await PostAsync("/Support/SendMessage", formData);
+
+            TestDocument<Message>.None(e => e.Text == formData["Text"] && e.SenderEmail == formData["Email"]);
+            Examine(ContentScheme.Html()
+                .Contains("Text", formData["Text"])
+                .Contains("Email", formData["Email"]));
+        }
+
+        [TestMethod]
+        public async Task Post_EmailIsEmpty_NoMessageCreated() {
+
+            var formData = new Dictionary<string, string> {
+                { "Text", "TestText" },
+                { "Email", "" }
+            };
+
+            await PostAsync("/Support/SendMessage", formData);
+
+            TestDocument<Message>.None(e => e.Text == formData["Text"] && e.SenderEmail == formData["Email"]);
+            Examine(ContentScheme.Html()
+                .Contains("Text", formData["Text"])
+                .Contains("Email", formData["Email"]));
+        }
+
+        [TestMethod]
+        public async Task Post_EmailIsInvalid_NoMessageCreated() {
+
+            var formData = new Dictionary<string, string> {
+                { "Text", "TestText" },
+                { "Email", "InvalidEmail" }
+            };
+
+            await PostAsync("/Support/SendMessage", formData);
+
+            TestDocument<Message>.None(e => e.Text == formData["Text"] && e.SenderEmail == formData["Email"]);
+            Examine(ContentScheme.Html()
+                .Contains("Text", formData["Text"])
+                .Contains("Email", formData["Email"]));
+        }
+    }
+}
---
+++ SupplementStore/Controllers/SupportController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SupplementStore.Application.Args;
+using SupplementStore.Application.Services;
+using SupplementStore.ViewModels.Support;
+
+namespace SupplementStore.Controllers {
+
+    public class SupportController : Controller {
+
+        UserManager<IdentityUser> UserManager { get; }
+
+        IMessageService MessageService { get; }
+
+        public SupportController(
+            UserManager<IdentityUser> userManager,
+            IMessageService messageService) {
+
+            UserManager = userManager;
+            MessageService = messageService;
+        }
+
+        public IActionResult SendMessage() {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(SendMessageVM model) {
+
+            if (ModelState.IsValid == false)
+                return View(model);
+
+            string userId = UserManager.GetUserId(HttpContext.User)
+                ?? UserManager.FindByEmailAsync(model.Email).Result?.Id;
+
+            var result = MessageService.Create(new MessageCreateArgs {
+                Text = model.Text,
+                UserId = userId,
+                SenderEmail = model.Email
+            });
+
+            return RedirectToAction(nameof(SendMessage));
+        }
+    }
+}
