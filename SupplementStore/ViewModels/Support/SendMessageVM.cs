32023dd59a79c1a3a5c5736b83831d3354b6e9eb
Adrian <mitura55@interia.pl>
2022-05-07 03:17:39 +0200

    Create Support's SendMessageVM.

---
+++ SupplementStore/ViewModels/Support/SendMessageVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupplementStore.ViewModels.Support {
+
+    public class SendMessageVM {
+
+        [Required(ErrorMessage = "TextRequiredErrorMessage")]
+        [Display(Name = "TextLabel")]
+        public string Text { get; set; }
+
+        [Required(ErrorMessage = "EmailRequiredErrorMessage")]
+        [Email]
+        [Display(Name = "EmailLabel")]
+        public string Email { get; set; }
+    }
+}
