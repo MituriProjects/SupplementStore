e66b1eef775073b9c108366ab47c89fc5e1650fc
Adrian <mitura55@interia.pl>
2022-05-11 01:12:51 +0200

    Create MessageDetails.

---
+++ SupplementStore.Application/Models/MessageDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SupplementStore.Application.Models {
+
+    public class MessageDetails {
+
+        public string Email { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+    }
+}
