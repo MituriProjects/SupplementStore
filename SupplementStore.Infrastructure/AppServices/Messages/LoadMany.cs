8b9e27c5256da35a33b3d207ff6110729ef33fca
Adrian <mitura55@interia.pl>
2022-05-11 01:15:13 +0200

    Add a LoadMany method to MessageService.

---
+++ SupplementStore.Infrastructure/AppServices/Messages/LoadMany.cs
@@ -0,0 +1,17 @@
+﻿using SupplementStore.Application.Models;
+using SupplementStore.Infrastructure.ModelMapping;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SupplementStore.Infrastructure.AppServices.Messages {
+
+    public partial class MessageService {
+
+        public IEnumerable<MessageDetails> LoadMany() {
+
+            return MessageRepository.Entities
+                .OrderByDescending(e => e.CreatedOn)
+                .ToDetails(UserRepository);
+        }
+    }
+}
