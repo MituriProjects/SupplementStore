782372fafc12fc2298799567610d5b9248bcf569
Adrian <mitura55@interia.pl>
2022-05-11 01:01:58 +0200

    Add a CreatedOn property to Message.

--- SupplementStore.Domain/Messages/Message.cs
+++ SupplementStore.Domain/Messages/Message.cs
@@ -11,5 +11,7 @@ namespace SupplementStore.Domain.Messages {
         public string UserId { get; set; }
 
         public string Text { get; set; }
+
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
     }
 }
