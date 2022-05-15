1f508482a9bf00c2cd59ddcd92b269245c73c306
Adrian <mitura55@interia.pl>
2022-05-15 03:17:53 +0200

    Decorate Address's EditVM's properties with the Label instead of Display attribute.

--- SupplementStore/ViewModels/Address/EditVM.cs
+++ SupplementStore/ViewModels/Address/EditVM.cs
@@ -7,16 +7,16 @@ namespace SupplementStore.ViewModels.Address {
         public string Id { get; set; }
 
         [Required(ErrorMessage = "StreetRequiredErrorMessage")]
-        [Display(Name = "StreetLabel")]
+        [Label]
         public string Street { get; set; }
 
         [Required(ErrorMessage = "PostalCodeRequiredErrorMessage")]
         [RegularExpression(@"^\d{2}-\d{3}$", ErrorMessage = "RegularExpressionErrorMessage")]
-        [Display(Name = "PostalCodeLabel")]
+        [Label]
         public string PostalCode { get; set; }
 
         [Required(ErrorMessage = "CityRequiredErrorMessage")]
-        [Display(Name = "CityLabel")]
+        [Label]
         public string City { get; set; }
     }
 }
