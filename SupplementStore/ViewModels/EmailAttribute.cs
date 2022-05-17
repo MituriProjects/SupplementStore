22e7f719f49523fb63aa0148690a8cc3dc61c343
Adrian <mitura55@interia.pl>
2022-05-17 16:27:50 +0200

    Modify EmailAttribute to inherit from WrapperValidationAttribute.

--- SupplementStore/ViewModels/EmailAttribute.cs
+++ SupplementStore/ViewModels/EmailAttribute.cs
@@ -1,48 +1,12 @@
-﻿using Microsoft.Extensions.Localization;
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.Reflection;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Runtime.CompilerServices;
 
 namespace SupplementStore.ViewModels {
 
-    public class EmailAttribute : ValidationAttribute {
+    public class EmailAttribute : WrapperValidationAttribute {
 
-        Type Type { get; }
-
-        public EmailAttribute([CallerFilePath]string filePath = null) {
-
-            var assemblyName = Assembly
-                .GetExecutingAssembly()
-                .GetName()
-                .Name;
-
-            var viewModelsPathPiece = assemblyName + "\\ViewModels";
-
-            var typeFullName = filePath
-                .Substring(filePath.IndexOf(viewModelsPathPiece), filePath.LastIndexOf(".") - filePath.IndexOf(viewModelsPathPiece))
-                .Replace("\\", ".");
-
-            Type = Assembly.GetExecutingAssembly().GetType(typeFullName);
-        }
-
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext) {
-
-            return new EmailAddressAttribute().IsValid(value)
-                ? ValidationResult.Success
-                : new ValidationResult(GetErrorMessage(validationContext));
-        }
-
-        private string GetErrorMessage(ValidationContext validationContext) {
-
-            var localizer = validationContext.GetService(typeof(IStringLocalizer<>).MakeGenericType(Type)) as IStringLocalizer;
-
-            var errorMessageKey = "EmailErrorMessage";
-            var errorMessage = localizer[errorMessageKey];
-
-            return errorMessage == errorMessageKey
-                ? "Invalid e-mail."
-                : errorMessage;
+        public EmailAttribute([CallerFilePath]string filePath = null)
+            : base(new EmailAddressAttribute(), "EmailErrorMessage", "Invalid e-mail.", filePath) {
         }
     }
 }
