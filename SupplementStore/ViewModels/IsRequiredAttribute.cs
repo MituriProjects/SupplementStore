ac4ccabed2123dcb6287eeb84f7c0df0588d7340
Adrian <mitura55@interia.pl>
2022-05-17 16:28:36 +0200

    Modify IsRequiredAttribute to inherit from WrapperValidationAttribute.

--- SupplementStore/ViewModels/IsRequiredAttribute.cs
+++ SupplementStore/ViewModels/IsRequiredAttribute.cs
@@ -1,48 +1,12 @@
-﻿using Microsoft.Extensions.Localization;
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.Reflection;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Runtime.CompilerServices;
 
 namespace SupplementStore.ViewModels {
 
-    public class IsRequiredAttribute : RequiredAttribute {
+    public class IsRequiredAttribute : WrapperValidationAttribute {
 
-        Type Type { get; }
-
-        public IsRequiredAttribute([CallerFilePath]string filePath = null, [CallerMemberName]string callerName = null) {
-
-            var assemblyName = Assembly
-                .GetExecutingAssembly()
-                .GetName()
-                .Name;
-
-            var viewModelsPathPiece = assemblyName + "\\ViewModels";
-
-            var typeFullName = filePath
-                .Substring(filePath.IndexOf(viewModelsPathPiece), filePath.LastIndexOf(".") - filePath.IndexOf(viewModelsPathPiece))
-                .Replace("\\", ".");
-
-            Type = Assembly.GetExecutingAssembly().GetType(typeFullName);
-
-            ErrorMessage = $"{callerName}RequiredErrorMessage";
-        }
-
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext) {
-
-            var result = base.IsValid(value, validationContext);
-
-            if (result != null)
-                result.ErrorMessage = GetErrorMessage(validationContext);
-
-            return result;
-        }
-
-        private string GetErrorMessage(ValidationContext validationContext) {
-
-            var localizer = validationContext.GetService(typeof(IStringLocalizer<>).MakeGenericType(Type)) as IStringLocalizer;
-
-            return localizer[ErrorMessage];
+        public IsRequiredAttribute([CallerFilePath]string filePath = null, [CallerMemberName]string callerName = null)
+            : base(new RequiredAttribute(), $"{callerName}RequiredErrorMessage", $"Required {callerName}.", filePath) {
         }
     }
 }
