0d21d0f6ab9fffa1523e7e3128ebb6b6a0d5e181
Adrian <mitura55@interia.pl>
2022-05-17 16:44:32 +0200

    Create PriceAttribute.

---
+++ SupplementStore/ViewModels/PriceAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace SupplementStore.ViewModels {
+
+    public class PriceAttribute : WrapperValidationAttribute {
+
+        public PriceAttribute([CallerFilePath]string filePath = null)
+            : base(new RangeAttribute(0.01, double.MaxValue), "PriceAboveZeroErrorMessage", "Price above zero required.", filePath) {
+        }
+    }
+}
