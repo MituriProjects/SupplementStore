f4b4d336c6a084882c5998474104e404e4c1a61d
Adrian <mitura55@interia.pl>
2022-05-17 07:59:15 +0200

    Add a generic IStringLocalizer dependency to Startup.

--- SupplementStore/Startup.cs
+++ SupplementStore/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
 using SupplementStore.Controllers.Services;
 using SupplementStore.DependencyResolving;
 using SupplementStore.Infrastructure;
@@ -61,6 +62,8 @@ namespace SupplementStore {
                     return new FlexibleStringLocalizer(type, factory);
                 };
             });
+
+            services.AddTransient(typeof(IStringLocalizer<>), typeof(FlexibleStringLocalizer<>));
 
             ReconfigureServices(services);
         }
