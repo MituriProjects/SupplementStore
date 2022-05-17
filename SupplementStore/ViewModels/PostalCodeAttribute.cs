36a3c2296a1855b1f709bb6fd1cc431731890e74
Adrian <mitura55@interia.pl>
2022-05-17 16:29:34 +0200

    Create PostalCodeAttribute.

---
+++ SupplementStore/ViewModels/PostalCodeAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace SupplementStore.ViewModels {
+
+    public class PostalCodeAttribute : WrapperValidationAttribute {
+
+        public PostalCodeAttribute([CallerFilePath]string filePath = null)
+            : base(new RegularExpressionAttribute(@"^\d{2}-\d{3}$"), "InvalidPostalCodeErrorMessage", "Invalid postal code.", filePath) {
+        }
+    }
+}
