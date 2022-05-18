8331f7e3b8e520dc2f937d317b233c5e849e9671
Adrian <mitura55@interia.pl>
2022-05-18 04:26:25 +0200

    Create a UserEntity's WithUserId extension method.

---
+++ SupplementStore.Tests/UserEntityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SupplementStore.Domain;
+
+namespace SupplementStore.Tests {
+
+    static class UserEntityExtensions {
+
+        public static TUserEntity WithUserId<TUserEntity>(this TUserEntity userEntity, IdentityUser user)
+            where TUserEntity : UserEntity {
+
+            userEntity.UserId = user.Id;
+
+            return userEntity;
+        }
+    }
+}
