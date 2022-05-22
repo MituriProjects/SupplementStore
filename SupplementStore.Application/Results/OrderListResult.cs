b259f87b1d8182735899ac66d7ef02aede3a5c17
Adrian <mitura55@interia.pl>
2022-05-23 01:47:28 +0200

    Create OrderListResult.

---
+++ SupplementStore.Application/Results/OrderListResult.cs
@@ -0,0 +1,12 @@
+﻿using SupplementStore.Application.Models;
+using System.Collections.Generic;
+
+namespace SupplementStore.Application.Results {
+
+    public class OrderListResult {
+
+        public int AllOrdersCount { get; set; }
+
+        public IEnumerable<OrderDetails> Orders { get; set; }
+    }
+}
