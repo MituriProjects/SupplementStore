77910ba6b8562024f5465f19e1a393f45176c577
Adrian <mitura55@interia.pl>
2022-05-23 01:51:27 +0200

    Modify Admin's OrdersVM to use PageVM.

--- SupplementStore/ViewModels/Admin/OrdersVM.cs
+++ SupplementStore/ViewModels/Admin/OrdersVM.cs
@@ -1,12 +1,15 @@
 ﻿using SupplementStore.Application.Models;
+using SupplementStore.ViewModels.Shared;
 using System.Collections.Generic;
 
 namespace SupplementStore.ViewModels.Admin {
 
     public class OrdersVM {
 
+        public PageVM Page { get; set; } = new PageVM();
+
         public IEnumerable<OrderDetails> OrderDetails { get; set; }
 
-        public IDictionary<string, string> UserEmails { get; set; }
+        public IDictionary<string, string> UserEmails { get; set; } = new Dictionary<string, string>();
     }
 }
