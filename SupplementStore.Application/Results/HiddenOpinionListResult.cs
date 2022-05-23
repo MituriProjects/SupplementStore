9a9e52dbf8a56d5671b3f7dfb595adce21eb8835
Adrian <mitura55@interia.pl>
2022-05-23 13:28:56 +0200

    Create HiddenOpinionListResult.

---
+++ SupplementStore.Application/Results/HiddenOpinionListResult.cs
@@ -0,0 +1,12 @@
+﻿using SupplementStore.Application.Models;
+using System.Collections.Generic;
+
+namespace SupplementStore.Application.Results {
+
+    public class HiddenOpinionListResult {
+
+        public int AllHiddenOpinionsCount { get; set; }
+
+        public IEnumerable<HiddenOpinionDetails> HiddenOpinions { get; set; }
+    }
+}
