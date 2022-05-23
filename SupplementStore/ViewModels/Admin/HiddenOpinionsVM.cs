f50a668cf820fb74b53957368b334b2591be7d5c
Adrian <mitura55@interia.pl>
2022-05-23 13:34:32 +0200

    Create Admin's HiddenOpinionsVM.

---
+++ SupplementStore/ViewModels/Admin/HiddenOpinionsVM.cs
@@ -0,0 +1,13 @@
+﻿using SupplementStore.Application.Models;
+using SupplementStore.ViewModels.Shared;
+using System.Collections.Generic;
+
+namespace SupplementStore.ViewModels.Admin {
+
+    public class HiddenOpinionsVM {
+
+        public PageVM Page { get; set; } = new PageVM();
+
+        public IEnumerable<HiddenOpinionDetails> HiddenOpinions { get; set; }
+    }
+}
