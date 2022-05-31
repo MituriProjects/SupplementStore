3681ea82b02b78e351036732ad368773037ae463
Adrian Mitura <kontakt@adrianmitura.pl>
2022-05-31 03:05:48 +0200

    Create an AddressApi's Get action.

---
+++ SupplementStore.Tests/Integration/AddressApiTests/GetTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SupplementStore.Domain.Addresses;
+using System.Threading.Tasks;
+
+namespace SupplementStore.Tests.Integration.AddressApiTests {
+
+    [TestClass]
+    public class GetTests : IntegrationTestsBase {
+
+        [TestMethod]
+        public async Task UserIsLoggedOut_ReturnsEmptyCollection() {
+
+            await GetAsync("api/address");
+
+            Examine("[]");
+        }
+
+        [TestMethod]
+        public async Task UserIsLoggedIn_ReturnsUserAddresses() {
+
+            var addresses = TestEntity.Random<Address>(4);
+            addresses[1]
+                .WithUserId(TestData.User);
+            addresses[3]
+                .WithUserId(TestData.User);
+
+            await GetAsync("api/address", TestData.User);
+
+            Examine(ContentScheme.Json()
+                .Contains("Id", addresses[1].AddressId)
+                .Contains("Street", addresses[1].Street)
+                .Contains("PostalCode", addresses[1].PostalCode.Value)
+                .Contains("City", addresses[1].City)
+                .Contains("Id", addresses[3].AddressId)
+                .Contains("Street", addresses[3].Street)
+                .Contains("PostalCode", addresses[3].PostalCode.Value)
+                .Contains("City", addresses[3].City));
+        }
+    }
+}
---
+++ SupplementStore/Controllers/AddressApiController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SupplementStore.Application.Models;
+using SupplementStore.Application.Services;
+using System.Collections.Generic;
+
+namespace SupplementStore.Controllers {
+
+    [ApiController]
+    [Route("api/address")]
+    public class AddressApiController : ControllerBase {
+
+        UserManager<IdentityUser> UserManager { get; }
+
+        IAddressService AddressService { get; }
+
+        public AddressApiController(
+            UserManager<IdentityUser> userManager, 
+            IAddressService addressService) {
+
+            UserManager = userManager;
+            AddressService = addressService;
+        }
+
+        [HttpGet]
+        public IEnumerable<AddressDetails> Get() {
+
+            var userId = UserManager.GetUserId(HttpContext.User);
+
+            if (userId == null)
+                return new AddressDetails[] { };
+
+            return AddressService.LoadMany(userId);
+        }
+    }
+}
